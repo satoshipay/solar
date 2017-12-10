3b45f5606e3cc7df9be650d3b862f446632e7295
Andy Wermke <andy@dev.next-step-software.com>
2017-12-10 14:00:48 +0100

    Divide wallet list into pubnet & testnet

--- src/components/WalletList.js
+++ src/components/WalletList.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import Divider from 'material-ui/Divider'
 import { List, ListItem } from 'material-ui/List'
 import Subheader from 'material-ui/Subheader'
 import { withRouter } from 'react-router-dom'
 
-const WalletList = ({ history, wallets }) => (
-  <List>
-    <Subheader>Wallets</Subheader>
-    {wallets.map(wallet => (
-      <ListItem key={wallet.id} primaryText={wallet.name} onClick={() => history.push(`/wallet/${wallet.id}`)} />
-    ))}
-  </List>
-)
+const WalletList = ({ history, wallets }) => {
+  const pubnetWallets = wallets.filter(wallet => !wallet.testnet)
+  const testnetWallets = wallets.filter(wallet => wallet.testnet)
+
+  const WalletListItem = wallet => (
+    <ListItem key={wallet.id} primaryText={wallet.name} onClick={() => history.push(`/wallet/${wallet.id}`)} />
+  )
+
+  const renderedPubnetWallets = pubnetWallets.length === 0 ? null : (
+    <div>
+      <Subheader>Wallets</Subheader>
+      {pubnetWallets.map(WalletListItem)}
+    </div>
+  )
+  const renderedTestnetWallets = testnetWallets.length === 0 ? null : (
+    <div>
+      <Subheader>Testnet Wallets</Subheader>
+      {testnetWallets.map(WalletListItem)}
+    </div>
+  )
+
+  return (
+    <List>
+      {renderedPubnetWallets}
+      {renderedPubnetWallets && renderedTestnetWallets ? <Divider /> : null}
+      {renderedTestnetWallets}
+    </List>
+  )
+}
 
 export default withRouter(WalletList)
