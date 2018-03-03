1927bfe947d99611fcf827065081f926513a421d
Andy Wermke <andy@dev.next-step-software.com>
2018-03-03 10:29:56 +0100

    Minor style improvements to wallet page

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 import { Card, CardText, CardTitle } from 'material-ui/Card'
+import Divider from 'material-ui/Divider'
 import { DetailData, DetailDataSet } from '../components/Data'
 import { AccountBalance } from '../components/LumenBalance'
 import TransactionList from '../components/TransactionList'
@@ -10,7 +11,7 @@ const WalletPage = ({ match: { params }, wallets }) => {
   if (!wallet) throw new Error(`Wallet not found. ID: ${params.id}`)
 
   return (
-    <Card>
+    <Card style={{ boxShadow: 'none' }}>
       <CardTitle title={wallet.name} subtitle={wallet.testnet ? 'Testnet' : null} />
       <CardText>
         <DetailDataSet>
@@ -21,6 +22,7 @@ const WalletPage = ({ match: { params }, wallets }) => {
       {/* TODO: Add "edit" icon button */}
       {/* TODO: Add action buttons (send payment, ...) (in <CardActions>) */}
       {/* TODO: Add advanced actions (backup, delete, merge, ...) */}
+      <Divider style={{ marginTop: 20, marginBottom: 20 }} />
       <TransactionList title='Recent transactions' publicKey={wallet.publicKey} testnet={wallet.testnet} />
     </Card>
   )
