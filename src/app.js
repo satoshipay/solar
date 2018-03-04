98cf76cda5de3a6f70eaf138c74b608190ced669
Andy Wermke <andy@dev.next-step-software.com>
2018-03-04 20:20:06 +0100

    Use HashRouter, since BrowserRouter won't work with production electron app
    Due to the file:// URL.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route
 } from 'react-router-dom'
 import { withProps } from 'recompose'
