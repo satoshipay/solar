75a522d0f02f796d30666df958eaed86dcea9a67
Andy Wermke <andy@dev.next-step-software.com>
2018-06-22 16:29:26 +0200

    Improve TransactionSummary, extend its story

--- stories/TransactionSummary.tsx
+++ stories/TransactionSummary.tsx
@@ -6,7 +6,7 @@ import { Transactions } from '../src/components/Subscribers'
 import TransactionSummary from '../src/components/TransactionSummary'
 
 storiesOf('TransactionSummary', module)
-  .add('default', () => {
+  .add('Payment with memo', () => {
     Network.useTestNetwork()
     const horizon = new Server('https://horizon-testnet.stellar.org')
 
@@ -17,6 +17,30 @@ storiesOf('TransactionSummary', module)
         amount: '1.5',
         asset: Asset.native(),
         destination: 'GA2CZKBI2C55WHALSTNPG54FOQCLC6Y4EIATZEIJOXWQPSEGN4CWAXFT'
+      }))
+      return builder.build()
+    })()
+
+    return (
+      <Async
+        promise={promise}
+        then={transaction => <TransactionSummary transaction={transaction} />}
+      />
+    )
+  })
+  .add('Account creation & Inflation destination', () => {
+    Network.useTestNetwork()
+    const horizon = new Server('https://horizon-testnet.stellar.org')
+
+    const promise = (async () => {
+      const account = await horizon.loadAccount('GBPBFWVBADSESGADWEGC7SGTHE3535FWK4BS6UW3WMHX26PHGIH5NF4W')
+      const builder = new TransactionBuilder(account, { memo: Memo.text('Demo transaction') })
+      builder.addOperation(Operation.createAccount({
+        startingBalance: '1.0',
+        destination: 'GA2CZKBI2C55WHALSTNPG54FOQCLC6Y4EIATZEIJOXWQPSEGN4CWAXFT'
+      }))
+      builder.addOperation(Operation.setOptions({
+        inflationDest: 'GCCD6AJOYZCUAQLX32ZJF2MKFFAUJ53PVCFQI3RHWKL3V47QYE2BNAUT'
       }))
       return builder.build()
     })()
