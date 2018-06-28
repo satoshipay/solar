577204e94b258a9cd25a2c5f5913820ac38d7590
Andy Wermke <andy@dev.next-step-software.com>
2018-06-28 16:14:25 +0200

    Add story for Balance of unactivated account

--- stories/Balance.tsx
+++ stories/Balance.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { AccountBalance } from '../src/components/Balance'
 
-storiesOf('AccountBalance', module)
-  .add('default', () => (
+storiesOf('Balance', module)
+  .add('AccountBalance activated', () => (
     <div>
       Current balance: <AccountBalance publicKey='GBPBFWVBADSESGADWEGC7SGTHE3535FWK4BS6UW3WMHX26PHGIH5NF4W' testnet />
+    </div>
+  ))
+  .add('AccountBalance unactivated', () => (
+    <div>
+      Current balance: <AccountBalance publicKey='GD52DFJ57XWSBCN3MZQ4Z2TO4TCVVP2UXVWCBSTCKDUYXVPGMSVKS4M5' testnet />
     </div>
   ))
