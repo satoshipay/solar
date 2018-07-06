1d43c7de9d894e2b7478f09d0ae3372d0ed1fc46
Andy Wermke <andy@dev.next-step-software.com>
2018-07-06 16:22:12 +0200

    Chop down CreatePayment dialog

--- src/components/Dialog/CreatePayment.tsx
+++ src/components/Dialog/CreatePayment.tsx
@@ -1,33 +1,14 @@
 import React from "react"
 import Dialog from "@material-ui/core/Dialog"
 import DialogContent from "@material-ui/core/DialogContent"
-import Drawer from "@material-ui/core/Drawer"
-import Card from "@material-ui/core/Card"
-import CardContent from "@material-ui/core/CardContent"
-import Typography from "@material-ui/core/Typography"
-import CloseIcon from "react-icons/lib/md/close"
 import { Transaction } from "stellar-sdk"
 import { createTransaction } from "../../lib/transaction"
 import { Account } from "../../stores/accounts"
 import { withHorizon, HorizonProps } from "../../hocs"
-import CreatePaymentForm, { PaymentCreationValues } from "../Form/CreatePayment"
-import TxConfirmationForm from "../Form/TxConfirmation"
+import { PaymentCreationValues } from "../Form/CreatePayment"
 import SubmissionProgress from "../SubmissionProgress"
-
-const CloseButton = (props: { onClick: (event: React.MouseEvent) => any }) => {
-  const style: React.CSSProperties = {
-    position: "absolute",
-    top: 16,
-    right: 24,
-    cursor: "pointer",
-    lineHeight: 0
-  }
-  return (
-    <div style={style} onClick={props.onClick}>
-      <CloseIcon style={{ width: 32, height: 32 }} />
-    </div>
-  )
-}
+import PaymentFormDrawer from "./PaymentForm"
+import TxConfirmationDrawer from "./TransactionConfirmation"
 
 const SubmissionProgressOverlay = (props: {
   open: boolean
@@ -39,75 +20,6 @@ const SubmissionProgressOverlay = (props: {
         <SubmissionProgress promise={props.submissionPromise} />
       </DialogContent>
     </Dialog>
-  )
-}
-
-interface PaymentFormDrawerProps {
-  account: Account
-  open: boolean
-  onClose: () => void
-  onSubmit: (values: PaymentCreationValues) => void
-}
-
-const PaymentFormDrawer = (props: PaymentFormDrawerProps) => {
-  return (
-    <Drawer open={props.open} anchor="right" onClose={props.onClose}>
-      <Card
-        style={{
-          position: "relative",
-          height: "100%",
-          padding: "0 12px",
-          width: "90vw",
-          maxWidth: "700px"
-        }}
-      >
-        <CloseButton onClick={props.onClose} />
-        <CardContent>
-          <Typography variant="headline" component="h2">
-            Send payment
-          </Typography>
-          <Typography gutterBottom variant="subheading" component="h3">
-            {props.account.testnet ? "Testnet" : null}
-          </Typography>
-          <div style={{ marginTop: 32 }}>
-            <CreatePaymentForm onSubmit={props.onSubmit} />
-          </div>
-        </CardContent>
-      </Card>
-    </Drawer>
-  )
-}
-
-interface TxConfirmationDrawerProps {
-  account: Account
-  open: boolean
-  transaction: Transaction | null
-  onClose: () => void
-  onSubmitTransaction: (tx: Transaction) => void
-}
-
-const TxConfirmationDrawer = (props: TxConfirmationDrawerProps) => {
-  return (
-    <Drawer open={props.open} anchor="right" onClose={props.onClose}>
-      <Card style={{ position: "relative", height: "100%", padding: "0 12px" }}>
-        <CardContent>
-          <Typography variant="headline" component="h2">
-            Confirm payment
-          </Typography>
-          <Typography gutterBottom variant="subheading" component="h3">
-            {props.account.testnet ? "Testnet" : null}
-          </Typography>
-          {props.transaction ? (
-            <TxConfirmationForm
-              transaction={props.transaction}
-              account={props.account}
-              onConfirm={props.onSubmitTransaction}
-              onCancel={props.onClose}
-            />
-          ) : null}
-        </CardContent>
-      </Card>
-    </Drawer>
   )
 }
 
---
+++ src/components/Dialog/TransactionConfirmation.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import Drawer from "@material-ui/core/Drawer"
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import Typography from "@material-ui/core/Typography"
+import { Transaction } from "stellar-sdk"
+import { Account } from "../../stores/accounts"
+import TxConfirmationForm from "../Form/TxConfirmation"
+
+interface TxConfirmationDrawerProps {
+  account: Account
+  open: boolean
+  transaction: Transaction | null
+  onClose: () => void
+  onSubmitTransaction: (tx: Transaction) => void
+}
+
+const TxConfirmationDrawer = (props: TxConfirmationDrawerProps) => {
+  return (
+    <Drawer open={props.open} anchor="right" onClose={props.onClose}>
+      <Card style={{ position: "relative", height: "100%", padding: "0 12px" }}>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            Confirm payment
+          </Typography>
+          <Typography gutterBottom variant="subheading" component="h3">
+            {props.account.testnet ? "Testnet" : null}
+          </Typography>
+          {props.transaction ? (
+            <TxConfirmationForm
+              transaction={props.transaction}
+              account={props.account}
+              onConfirm={props.onSubmitTransaction}
+              onCancel={props.onClose}
+            />
+          ) : null}
+        </CardContent>
+      </Card>
+    </Drawer>
+  )
+}
+
+export default TxConfirmationDrawer
