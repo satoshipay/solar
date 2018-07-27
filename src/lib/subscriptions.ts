d04256e65c6bcfbaeb57c85ae8e708132bf04278
Andy Wermke <andy.dev@next-step-software.com>
2018-07-27 12:24:13 +0200

    Don't show account stream errors if the stream still seems to work

--- src/lib/subscriptions.ts
+++ src/lib/subscriptions.ts
@@ -35,7 +35,7 @@ function createAccountObservable(horizon: Server, accountPubKey: string) {
 
   const subscribeToAccountDataStream = () => {
     let lastMessageJson = ""
-    let lastErrorJson = ""
+    let lastMessageTime = 0
 
     horizon
       .accounts()
@@ -49,17 +49,21 @@ function createAccountObservable(horizon: Server, accountPubKey: string) {
             lastMessageJson = serialized
             Object.assign(accountObservable, accountData)
           }
+          lastMessageTime = Date.now()
         },
         onerror(error: any) {
-          const serialized = JSON.stringify(error)
-          if (serialized !== lastErrorJson) {
-            // Deduplicate errors. Every few seconds there is a new useless error with the same data as the previous.
-            lastErrorJson = serialized
-            addError(new Error("Account data update stream errored."))
-
-            // tslint:disable-next-line:no-console
-            console.error(error)
-          }
+          setTimeout(() => {
+            if (Date.now() - lastMessageTime > 2500) {
+              // Every few seconds there is a new useless error with the same data as the previous.
+              // So only show them if there is no successful message afterwards :-/
+              addError(new Error("Account data update stream errored."))
+            } else {
+              console.warn(
+                "Account data update stream had an error, but still seems to work fine:",
+                error
+              )
+            }
+          }, 2500)
         }
       } as any)
   }
