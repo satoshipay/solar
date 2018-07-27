1f24b4d8505c017a545d93a7589107d47276cf4c
Andy Wermke <andy.dev@next-step-software.com>
2018-07-27 12:23:14 +0200

    Move removeErrorByID() call to errors store
    Since `onExited` cannot be trusted if there are concurrent errors.

--- src/stores/errors.ts
+++ src/stores/errors.ts
@@ -1,8 +1,8 @@
 import { observable, IObservableArray } from "mobx"
 
 export interface ErrorItem {
-  id: number
   error: any
+  id: number
 }
 
 const ErrorsStore: IObservableArray<ErrorItem> = observable([])
@@ -11,17 +11,22 @@ export default ErrorsStore
 
 let nextID = 1
 
+function removeErrorByID(errorID: number) {
+  const index = ErrorsStore.findIndex(errorItem => errorItem.id === errorID)
+  ErrorsStore.splice(index, 1)
+}
+
 export function addError(error: any) {
   ErrorsStore.push({
     id: nextID++,
     error
   })
 
+  // just to prevent memory leaks
+  setTimeout(() => {
+    removeErrorByID(error.id)
+  }, 10000)
+
   // tslint:disable-next-line:no-console
   console.error(error)
-}
-
-export function removeErrorByID(errorID: number) {
-  const index = ErrorsStore.findIndex(errorItem => errorItem.id === errorID)
-  ErrorsStore.splice(index, 1)
 }
