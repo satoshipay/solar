b0cb3bac2f1503e01ed9ab664d22304a4fd6c855
Andy Wermke <andy.dev@next-step-software.com>
2018-08-09 12:08:32 +0200

    Define win drag region of frameless window, add more padding

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,9 +1,23 @@
 import React from "react"
 import { Box } from "./Box"
 
-const Section = (props: { children: React.ReactNode; style?: React.CSSProperties }) => {
+const FramelessWindowInvisibleTitleBar = () => {
+  if (process.env.PLATFORM === "darwin") {
+    return (
+      <>
+        <div className="mac-frameless-window-invisible-title-bar" />
+        <div style={{ width: "100%", padding: "12px 0 0", margin: 0 }} />
+      </>
+    )
+  } else {
+    return null
+  }
+}
+
+const Section = (props: { children: React.ReactNode; style?: React.CSSProperties; top?: boolean }) => {
   return (
     <Box component="section" padding={16} style={props.style}>
+      {top ? <FramelessWindowInvisibleTitleBar /> : null}
       {props.children}
     </Box>
   )
--- src/pages/qr-scanner.tsx
+++ src/pages/qr-scanner.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Section } from "../components/Layout/Page"
 
-const QRScannerPage = () => <Section>QR scanner goes here.</Section>
+const QRScannerPage = () => <Section top>QR scanner goes here.</Section>
 
 export default QRScannerPage
