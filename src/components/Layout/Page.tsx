d794ae0a86a79f37ab7ce8fd51b0c91352c16c2b
Andy Wermke <andy.dev@next-step-software.com>
2018-08-10 16:37:05 +0200

    Fade out window content at the top in frameless window

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { Box } from "./Box"
 
-const FramelessWindowInvisibleTitleBar = () => {
+const FramelessWindowInvisibleTitleBar = (props: { backgroundColor?: React.CSSProperties["backgroundColor"] }) => {
   if (process.env.PLATFORM === "darwin") {
     // Add invisible window-drag area and a bit of additional v-space on top
+    // Need to define a static CSS class for it, since `-webkit-app-region` in CSS-in-JS might lead to trouble
+    const background = props.backgroundColor ? `linear-gradient(${props.backgroundColor}, transparent)` : undefined
     return (
       <>
-        <div className="mac-frameless-window-invisible-title-bar" />
+        <div className="mac-frameless-window-invisible-title-bar" style={{ background }} />
         <div style={{ width: "100%", padding: "4px 0 0", margin: 0 }} />
       </>
     )
@@ -15,10 +17,16 @@ const FramelessWindowInvisibleTitleBar = () => {
   }
 }
 
-const Section = (props: { children: React.ReactNode; style?: React.CSSProperties; top?: boolean }) => {
+interface SectionProps {
+  children: React.ReactNode
+  backgroundColor?: React.CSSProperties["backgroundColor"]
+  top?: boolean
+}
+
+const Section = (props: SectionProps) => {
   return (
-    <Box component="section" padding={16} style={props.style}>
-      {props.top ? <FramelessWindowInvisibleTitleBar /> : null}
+    <Box component="section" padding={16} style={{ backgroundColor: props.backgroundColor }}>
+      {props.top ? <FramelessWindowInvisibleTitleBar backgroundColor={props.backgroundColor} /> : null}
       {props.children}
     </Box>
   )
