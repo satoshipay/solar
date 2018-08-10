5a2d5ebf35ac63755b71835171fa7a95b083aee3
Andy Wermke <andy.dev@next-step-software.com>
2018-08-10 15:22:37 +0200

    Use a separate key store for electron development (#96)

--- electron/lib/storage.js
+++ electron/lib/storage.js
@@ -1,7 +1,11 @@
 const { app, ipcMain } = require("electron")
+const isDev = require("electron-is-dev")
 const Store = require("electron-store")
 
-const mainStore = new Store()
+// Use different key stores for development and production
+const mainStore = new Store({
+  name: isDev ? "development" : "config"
+})
 
 ipcMain.on("storage:keys:readSync", event => {
   event.returnValue = mainStore.has("keys") ? mainStore.get("keys") : {}
