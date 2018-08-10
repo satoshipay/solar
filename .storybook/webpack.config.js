c75f5c5f5f7805084129dd07ec797ec81915b8e6
Andy Wermke <andy.dev@next-step-software.com>
2018-08-10 11:49:32 +0200

    Fix storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,13 +1,22 @@
+const path = require("path")
+const webpack = require("webpack")
+
 // Source: <https://medium.com/@mtiller/storybook-react-typescript-and-jest-c9059ea06fa7>
-const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js')
+const genDefaultConfig = require("@storybook/react/dist/server/config/defaults/webpack.config.js")
 
 module.exports = (baseConfig, env) => {
   const config = genDefaultConfig(baseConfig, env)
 
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve('awesome-typescript-loader')
+    loader: require.resolve("awesome-typescript-loader")
   })
-  config.resolve.extensions.push('.ts', '.tsx')
+  config.plugins.push(
+    new webpack.NormalModuleReplacementPlugin(/\/platform\/key-store$/, resource => {
+      resource.request = resource.request.replace("/platform/key-store", "/platform/web/key-store")
+    })
+  )
+  config.resolve.extensions.push(".ts", ".tsx")
+
   return config
 }
