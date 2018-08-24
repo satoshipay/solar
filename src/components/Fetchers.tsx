5e7578e2711a4311af3a1d75a0a4d2b1d070c31d
Andy Wermke <andy.dev@next-step-software.com>
2018-08-24 16:32:37 +0200

    Add Fetchers.tsx

---
+++ src/components/Fetchers.tsx
@@ -0,0 +1,59 @@
+import React from "react"
+import Async from "react-promise"
+import { Horizon } from "./Subscribers"
+
+const memCache = new Map<string, any>()
+const memCacheCurrentlyFetching = new Map<string, boolean>()
+
+async function memoize(cacheKey: string, fetching: Promise<any>) {
+  memCacheCurrentlyFetching.set(cacheKey, true)
+  try {
+    return await fetching
+  } finally {
+    memCacheCurrentlyFetching.set(cacheKey, false)
+  }
+}
+
+interface MemoizedProps<Value> {
+  cacheKey: string
+  pending?: React.ReactNode
+  fetch: () => Promise<any>
+  then: (resolved: Value) => React.ReactNode
+  catch?: (rejection: Error) => React.ReactNode
+}
+
+const Memoized = <Value extends {}>(props: MemoizedProps<Value>) => {
+  if (memCache.has(props.cacheKey)) {
+    return <>{memCache.get(props.cacheKey)}</>
+  } else if (memCacheCurrentlyFetching.get(props.cacheKey)) {
+    return <>{props.pending}</>
+  }
+
+  return (
+    <Async
+      promise={memoize(props.cacheKey, props.fetch())}
+      then={props.then}
+      catch={props.catch}
+      pending={props.pending}
+    />
+  )
+}
+
+export const AccountName = (props: { publicKey: string; testnet: boolean }) => {
+  return (
+    <Horizon testnet={props.testnet}>
+      {horizon => (
+        <Memoized
+          cacheKey={`AccountData:${props.publicKey}`}
+          fetch={() =>
+            horizon
+              .accounts()
+              .accountId(props.publicKey)
+              .call()
+          }
+          then={(accountData: any) => accountData.home_domain || props.publicKey}
+        />
+      )}
+    </Horizon>
+  )
+}
