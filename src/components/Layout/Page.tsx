fae6ed76d66b1f8387160e0a41f0ec3c4c7b326b
Andy Wermke <andy.dev@next-step-software.com>
2018-08-30 12:00:33 +0200

    Fix frameless window gradient bar (#132)

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -6,12 +6,7 @@ const FramelessWindowInvisibleTitleBar = (props: { backgroundColor?: React.CSSPr
     // Add invisible window-drag area and a bit of additional v-space on top
     // Need to define a static CSS class for it, since `-webkit-app-region` in CSS-in-JS might lead to trouble
     const background = props.backgroundColor ? `linear-gradient(${props.backgroundColor}, transparent)` : undefined
-    return (
-      <>
-        <div className="mac-frameless-window-invisible-title-bar" style={{ background }} />
-        <div style={{ width: "100%", padding: "4px 0 0", margin: 0 }} />
-      </>
-    )
+    return <div className="mac-frameless-window-invisible-title-bar" style={{ background }} />
   } else {
     return null
   }
@@ -24,11 +19,20 @@ interface SectionProps {
 }
 
 const Section = (props: SectionProps) => {
+  const style: React.CSSProperties = {
+    backgroundColor: props.backgroundColor || "white",
+    position: "relative",
+    zIndex: props.top ? undefined : 1
+  }
   return (
-    <Box component="section" padding={16} style={{ backgroundColor: props.backgroundColor }}>
+    <>
       {props.top ? <FramelessWindowInvisibleTitleBar backgroundColor={props.backgroundColor} /> : null}
-      {props.children}
-    </Box>
+      <Box component="section" padding={16} style={style}>
+        {/* Add a little padding to the top if window is frameless */}
+        {props.top ? <div style={{ width: "100%", padding: "4px 0 0", margin: 0 }} /> : null}
+        {props.children}
+      </Box>
+    </>
   )
 }
 
