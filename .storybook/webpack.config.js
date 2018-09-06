ef0d2348ff8b57f1737ccd995bd24c8c68bf2aa2
Andy Wermke <andy.dev@next-step-software.com>
2018-09-06 17:12:23 +0200

    Fix deprecation warnings when running storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,22 +1,17 @@
 const path = require("path")
 const webpack = require("webpack")
 
-// Source: <https://medium.com/@mtiller/storybook-react-typescript-and-jest-c9059ea06fa7>
-const genDefaultConfig = require("@storybook/react/dist/server/config/defaults/webpack.config.js")
-
-module.exports = (baseConfig, env) => {
-  const config = genDefaultConfig(baseConfig, env)
-
-  config.module.rules.push({
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve("awesome-typescript-loader")
   })
-  config.plugins.push(
+  defaultConfig.plugins.push(
     new webpack.NormalModuleReplacementPlugin(/\/platform\/key-store$/, resource => {
       resource.request = resource.request.replace("/platform/key-store", "/platform/web/key-store")
     })
   )
-  config.resolve.extensions.push(".ts", ".tsx")
+  defaultConfig.resolve.extensions.push(".ts", ".tsx")
 
-  return config
+  return defaultConfig
 }
