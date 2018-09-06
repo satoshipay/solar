029bdad1002bbaf6a0fffd999995f5002aaebfd5
Andy Wermke <andy.dev@next-step-software.com>
2018-09-06 10:48:43 +0200

    Fix tx confirmation dialog title: Don't show "Payment" if it's not (#145)

--- src/components/Dialog/TransactionConfirmation.tsx
+++ src/components/Dialog/TransactionConfirmation.tsx
@@ -3,25 +3,33 @@ import Drawer from "@material-ui/core/Drawer"
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
-import { Transaction } from "stellar-sdk"
+import { Transaction, TransactionOperation } from "stellar-sdk"
 import { Account } from "../../stores/accounts"
 import TxConfirmationForm from "../Form/TxConfirmation"
+
+const isPaymentOperation = (operation: TransactionOperation) =>
+  ["createAccount", "payment"].indexOf(operation.type) > -1
 
 interface TxConfirmationDrawerProps {
   account: Account
   open: boolean
-  transaction: Transaction | null
+  transaction: Transaction
   onClose: () => void
   onSubmitTransaction: (tx: Transaction, formValues: { password: string | null }) => void
 }
 
 const TxConfirmationDrawer = (props: TxConfirmationDrawerProps) => {
+  const title =
+    props.transaction && props.transaction.operations.every(isPaymentOperation)
+      ? "Confirm Payment"
+      : "Confirm Transaction"
+
   return (
     <Drawer open={props.open} anchor="right" onClose={props.onClose}>
       <Card style={{ position: "relative", height: "100%", padding: "0 12px" }}>
         <CardContent>
           <Typography variant="headline" component="h2">
-            Confirm payment
+            {title}
           </Typography>
           <Typography gutterBottom variant="subheading" component="h3">
             {props.account.testnet ? "Testnet" : null}
