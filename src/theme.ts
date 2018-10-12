3c1f035850e0c0b940eaebd944afcd6e70ed32c5
Andy Wermke <andy.dev@next-step-software.com>
2018-10-12 12:54:50 +0200

    Change theme: button styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles"
+import grey from "@material-ui/core/colors/grey"
 import indigo from "@material-ui/core/colors/indigo"
 import lightBlue from "@material-ui/core/colors/lightBlue"
 
@@ -6,6 +7,16 @@ export const brandColor = indigo[500]
 
 const theme = createMuiTheme({
   overrides: {
+    MuiButton: {
+      root: {
+        borderRadius: 3
+      },
+      contained: {
+        backgroundColor: "white",
+        border: `1px solid ${grey["400"]}`,
+        boxShadow: "none"
+      }
+    },
     MuiFormLabel: {
       focused: {
         color: "inherit !important"
