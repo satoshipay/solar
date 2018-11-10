895ecd888aa17b6dc8f4bbeadad3af6a25d6bf97
Andy Wermke <andy.dev@next-step-software.com>
2018-11-10 11:13:17 +0100

    Fix account deletion (#249)

--- src/components/Dialog/AccountDeletion.tsx
+++ src/components/Dialog/AccountDeletion.tsx
@@ -7,7 +7,7 @@ import DialogContentText from "@material-ui/core/DialogContentText"
 import DialogTitle from "@material-ui/core/DialogTitle"
 import DeleteIcon from "@material-ui/icons/Delete"
 import WarnIcon from "@material-ui/icons/Warning"
-import { Account, AccountsContext } from "../../context/accounts"
+import { Account, AccountsConsumer } from "../../context/accounts"
 import AccountBalances from "../Account/AccountBalances"
 import Background from "../Background"
 import ButtonIconLabel from "../ButtonIconLabel"
@@ -15,46 +15,52 @@ import ButtonIconLabel from "../ButtonIconLabel"
 interface Props {
   account: Account
   open: boolean
-  deleteAccount: AccountsContext["deleteAccount"]
   onClose: () => void
   onDeleted: () => void
 }
 
 const AccountDeletionDialog = (props: Props) => {
-  const { deleteAccount } = props
-  const onConfirm = () => {
-    deleteAccount(props.account.id)
-    props.onClose()
-    props.onDeleted()
-  }
   return (
-    <Dialog open={props.open} onClose={props.onClose}>
-      <Background opacity={0.08}>
-        <WarnIcon style={{ fontSize: 160 }} />
-      </Background>
-      <DialogTitle>Confirm Account Deletion</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Are you sure you want to delete the account "{props.account.name}
-          "?
-          <br />
-          Make sure to backup your private key if there are still funds on the account!
-        </DialogContentText>
-        <DialogContentText style={{ marginTop: 16 }}>
-          Balance: <AccountBalances publicKey={props.account.publicKey} testnet={props.account.testnet} />
-        </DialogContentText>
-        <DialogActions style={{ marginTop: 16 }}>
-          <Button color="primary" onClick={props.onClose} style={{ marginRight: 16 }}>
-            Cancel
-          </Button>
-          <Button color="primary" variant="contained" autoFocus onClick={onConfirm}>
-            <ButtonIconLabel label="Delete">
-              <DeleteIcon />
-            </ButtonIconLabel>
-          </Button>
-        </DialogActions>
-      </DialogContent>
-    </Dialog>
+    <AccountsConsumer>
+      {({ deleteAccount }) => (
+        <Dialog open={props.open} onClose={props.onClose}>
+          <Background opacity={0.08}>
+            <WarnIcon style={{ fontSize: 160 }} />
+          </Background>
+          <DialogTitle>Confirm Account Deletion</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to delete the account "{props.account.name}
+              "?
+              <br />
+              Make sure to backup your private key if there are still funds on the account!
+            </DialogContentText>
+            <DialogContentText style={{ marginTop: 16 }}>
+              Balance: <AccountBalances publicKey={props.account.publicKey} testnet={props.account.testnet} />
+            </DialogContentText>
+            <DialogActions style={{ marginTop: 16 }}>
+              <Button color="primary" onClick={props.onClose} style={{ marginRight: 16 }}>
+                Cancel
+              </Button>
+              <Button
+                autoFocus
+                color="primary"
+                onClick={() => {
+                  deleteAccount(props.account.id)
+                  props.onClose()
+                  props.onDeleted()
+                }}
+                variant="contained"
+              >
+                <ButtonIconLabel label="Delete">
+                  <DeleteIcon />
+                </ButtonIconLabel>
+              </Button>
+            </DialogActions>
+          </DialogContent>
+        </Dialog>
+      )}
+    </AccountsConsumer>
   )
 }
 
