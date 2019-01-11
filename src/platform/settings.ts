26c210348932a7867d1159014c36e848fe125359
Andy Wermke <andy.dev@next-step-software.com>
2019-01-11 17:13:12 +0100

    Store and retrieve ignored pending txs

--- src/platform/settings.ts
+++ src/platform/settings.ts
@@ -3,7 +3,9 @@ import { SettingsData } from "./types"
 export { SettingsData }
 
 interface SettingsStore {
+  loadIgnoredSignatureRequestHashes(): string[]
   loadSettings(): Partial<SettingsData>
+  saveIgnoredSignatureRequestHashes(updatedSignatureRequestHashes: string[]): void
   saveSettings(settingsUpdate: Partial<SettingsData>): void
 }
 
@@ -19,6 +21,9 @@ function getImplementation(): SettingsStore {
     throw new Error("There is no implementation for your platform.")
   }
 }
+
+export const loadIgnoredSignatureRequestHashes = implementation.loadIgnoredSignatureRequestHashes
+export const saveIgnoredSignatureRequestHashes = implementation.saveIgnoredSignatureRequestHashes
 
 export const loadSettings = implementation.loadSettings
 export const saveSettings = implementation.saveSettings
