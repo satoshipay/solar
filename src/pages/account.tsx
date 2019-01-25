40f4ad277554fe34690089c3c8e7d3b126191106
Andy Wermke <andy.dev@next-step-software.com>
2019-01-25 15:07:30 +0000

    Give major dialogs their own routes (#365)

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,7 +1,5 @@
 import React from "react"
-import { useContext, useState } from "react"
-import { History } from "history"
-import { match } from "react-router"
+import { useContext } from "react"
 import Button from "@material-ui/core/Button"
 import CircularProgress from "@material-ui/core/CircularProgress"
 import Typography from "@material-ui/core/Typography"
@@ -17,6 +15,7 @@ import TransactionList from "../components/Account/TransactionList"
 import CreatePaymentDialog from "../components/Dialog/CreatePayment"
 import ManageAssetsDialog from "../components/Dialog/ManageAssets"
 import ManageSignersDialog from "../components/Dialog/ManageSigners"
+import ReceivePaymentDialog from "../components/Dialog/ReceivePayment"
 import { MinimumAccountBalance } from "../components/Fetchers"
 import QRCodeIcon from "../components/Icon/QRCode"
 import { HorizontalLayout } from "../components/Layout/Box"
@@ -25,9 +24,9 @@ import { Section } from "../components/Layout/Page"
 import { Account, AccountsContext } from "../context/accounts"
 import { SettingsContext } from "../context/settings"
 import { SignatureDelegationContext } from "../context/signatureDelegation"
-import { useAccountData, useHorizon, useRecentTransactions } from "../hooks"
+import { useAccountData, useHorizon, useRecentTransactions, useRouter } from "../hooks"
 import { hasSigned } from "../lib/transaction"
-import ReceivePaymentDialog from "../components/Dialog/ReceivePayment"
+import * as routes from "../routes"
 
 function DetailContent(props: { children: React.ReactNode; style?: React.CSSProperties }) {
   return (
@@ -154,22 +153,22 @@ function Transactions(props: { account: Account }) {
 }
 
 interface Props {
-  history: History
-  match: match<{ id: string }>
+  accountID: string
+  showAssetManagement: boolean
+  showCreatePayment: boolean
+  showReceivePayment: boolean
+  showSignersManagement: boolean
 }
 
 function AccountPage(props: Props) {
-  const { params } = props.match
-
   const { accounts, renameAccount } = useContext(AccountsContext)
-  const [isAssetsDrawerOpen, setAssetsDrawerOpen] = useState(false)
-  const [isPaymentDrawerOpen, setPaymentDrawerOpen] = useState(false)
-  const [isReceiveDrawerOpen, setReceiveDrawerOpen] = useState(false)
-  const [isSignersDrawerOpen, setSignersDrawerOpen] = useState(false)
+  const router = useRouter()
 
-  const account = accounts.find(someAccount => someAccount.id === params.id)
+  const onCloseDialog = () => router.history.push(routes.account(props.accountID))
+
+  const account = accounts.find(someAccount => someAccount.id === props.accountID)
   if (!account) {
-    throw new Error(`Wallet account not found. ID: ${params.id}`)
+    throw new Error(`Wallet account not found. ID: ${props.accountID}`)
   }
 
   return (
@@ -177,8 +176,8 @@ function AccountPage(props: Props) {
       <Section top brandColored grow={0}>
         <AccountHeaderCard
           account={account}
-          onManageAssets={() => setAssetsDrawerOpen(true)}
-          onManageSigners={() => setSignersDrawerOpen(true)}
+          onManageAssets={() => router.history.push(routes.manageAccountAssets(props.accountID))}
+          onManageSigners={() => router.history.push(routes.manageAccountSigners(props.accountID))}
           onRenameAccount={renameAccount}
         >
           <DetailContent style={{ marginTop: 12, marginLeft: 48 }}>
@@ -187,18 +186,18 @@ function AccountPage(props: Props) {
           <VerticalMargin size={40} />
           <AccountActions
             account={account}
-            onCreatePayment={() => setPaymentDrawerOpen(true)}
-            onReceivePayment={() => setReceiveDrawerOpen(true)}
+            onCreatePayment={() => router.history.push(routes.createPayment(props.accountID))}
+            onReceivePayment={() => router.history.push(routes.receivePayment(props.accountID))}
           />
         </AccountHeaderCard>
       </Section>
       <Section backgroundColor="#f6f6f6">
         <Transactions account={account} />
       </Section>
-      <CreatePaymentDialog account={account} open={isPaymentDrawerOpen} onClose={() => setPaymentDrawerOpen(false)} />
-      <ManageAssetsDialog account={account} open={isAssetsDrawerOpen} onClose={() => setAssetsDrawerOpen(false)} />
-      <ManageSignersDialog account={account} open={isSignersDrawerOpen} onClose={() => setSignersDrawerOpen(false)} />
-      <ReceivePaymentDialog account={account} open={isReceiveDrawerOpen} onClose={() => setReceiveDrawerOpen(false)} />
+      <CreatePaymentDialog account={account} open={props.showCreatePayment} onClose={onCloseDialog} />
+      <ManageAssetsDialog account={account} open={props.showAssetManagement} onClose={onCloseDialog} />
+      <ManageSignersDialog account={account} open={props.showSignersManagement} onClose={onCloseDialog} />
+      <ReceivePaymentDialog account={account} open={props.showReceivePayment} onClose={onCloseDialog} />
     </>
   )
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 export const allAccounts = () => "/"
 export const account = (accountID: string) => `/account/${accountID}`
-export const accountAssets = (accountID: string) => `/account/${accountID}/assets`
 export const createAccount = (testnet: boolean) => `/account/create/${testnet ? "testnet" : "mainnet"}`
+export const createPayment = (accountID: string) => `/account/${accountID}/send`
+export const receivePayment = (accountID: string) => `/account/${accountID}/receive`
+export const manageAccountAssets = (accountID: string) => `/account/${accountID}/assets`
 export const manageAccountSigners = (accountID: string) => `/account/${accountID}/signers`
 export const settings = () => "/settings"
