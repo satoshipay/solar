c239e945a11c5a2da72b430f870fc7696017bac1
Andy Wermke <andy.dev@next-step-software.com>
2019-01-31 11:52:21 +0000

    Merge Settings component into settings page file

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,16 +1,58 @@
 import React from "react"
+import { useContext } from "react"
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import Typography from "@material-ui/core/Typography"
+import { SettingsContext } from "../context/settings"
 import BackButton from "../components/BackButton"
 import { Box, HorizontalLayout, VerticalLayout } from "../components/Layout/Box"
+import ToggleSection from "../components/Layout/ToggleSection"
 import { Section } from "../components/Layout/Page"
-import Settings from "../components/Settings"
 import { useRouter } from "../hooks"
 import * as routes from "../routes"
 
 // tslint:disable-next-line
 const pkg = require("../../package.json")
+
+function Settings() {
+  const settings = useContext(SettingsContext)
+  return (
+    <>
+      <ToggleSection checked={settings.showTestnet} onChange={settings.toggleTestnet} title="Show Testnet Accounts">
+        <Typography
+          color={settings.showTestnet ? "default" : "textSecondary"}
+          style={{ margin: "12px 0 0" }}
+          variant="body1"
+        >
+          The test network is a copy of the main Stellar network were the traded tokens have no real-world value. You
+          can request free testnet XLM from the so-called friendbot to activate a testnet account and get started
+          without owning any actual funds.
+        </Typography>
+        <Typography
+          color={settings.showTestnet ? "default" : "textSecondary"}
+          style={{ margin: "12px 0 0" }}
+          variant="body1"
+        >
+          Note: Testnet accounts will always be shown if you have got testnet accounts already.
+        </Typography>
+      </ToggleSection>
+      <ToggleSection
+        checked={settings.multiSignature}
+        onChange={settings.toggleMultiSignature}
+        title="Enable Multi-Signature Features"
+      >
+        <Typography
+          color={settings.multiSignature ? "default" : "textSecondary"}
+          style={{ margin: "12px 0 0" }}
+          variant="body1"
+        >
+          <b>Experimental feature:</b> Add co-signers to an account, define that all signers of an account have to sign
+          transactions unanimously or a certain subset of signers have to sign a transaction in order to be valid.
+        </Typography>
+      </ToggleSection>
+    </>
+  )
+}
 
 function SettingsPage() {
   const router = useRouter()
