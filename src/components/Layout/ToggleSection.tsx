230769f0f10932cd6c159ea1f98b76d1655a415f
Andy Wermke <andy.dev@next-step-software.com>
2019-01-31 12:04:04 +0000

    Add `disabled` prop to ToggleSection

--- src/components/Layout/ToggleSection.tsx
+++ src/components/Layout/ToggleSection.tsx
@@ -6,20 +6,26 @@ import { Box } from "./Box"
 interface Props {
   checked: boolean
   children: React.ReactNode
+  disabled?: boolean
   title: React.ReactNode
   onChange?: () => void
   style?: React.CSSProperties
 }
 
 const ToggleSection = (props: Props) => {
+  const onChange = () => {
+    if (props.onChange && !props.disabled) {
+      props.onChange()
+    }
+  }
   return (
     <Box style={{ display: "flex", margin: "24px 0 0", ...props.style }}>
       <Box width={70} shrink={0}>
-        <Switch color="primary" checked={props.checked} onChange={props.onChange} />
+        <Switch color="primary" checked={props.checked} disabled={props.disabled} onChange={props.onChange} />
       </Box>
       <Box grow shrink={0} width="calc(100% - 70px)">
         <Typography variant="title" style={{ marginTop: 12 }}>
-          <span onClick={props.onChange} style={{ cursor: "pointer" }}>
+          <span onClick={onChange} style={{ cursor: "pointer" }}>
             {props.title}
           </span>
         </Typography>
