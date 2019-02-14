f3a80e00701e1a20eb9023c082457fbba3a569af
Andy Wermke <andy.dev@next-step-software.com>
2019-02-14 18:43:35 +0000

    Show local account name rather than public key

--- src/components/PublicKey.tsx
+++ src/components/PublicKey.tsx
@@ -1,5 +1,7 @@
 import React from "react"
+import { useContext } from "react"
 import Typography from "@material-ui/core/Typography"
+import { AccountsContext } from "../context/accounts"
 
 type Variant = "full" | "short" | "shorter"
 
@@ -23,17 +25,33 @@ interface Props {
   style?: React.CSSProperties
 }
 
-const PublicKey = ({ publicKey, style, variant }: Props) => {
-  const digits = getDigitCounts(variant)
+const PublicKey = (props: Props) => {
+  const digits = getDigitCounts(props.variant)
+  const { accounts } = useContext(AccountsContext)
 
-  if (publicKey.length !== 56) {
-    return <>{publicKey}</>
+  const matchingLocalAccount = accounts.find(account => account.publicKey === props.publicKey)
+  const style: React.CSSProperties = { display: "inline", fontSize: "inherit", fontWeight: "bold", ...props.style }
+
+  if (props.publicKey.length !== 56) {
+    return <>{props.publicKey}</>
+  } else if (matchingLocalAccount) {
+    // Note: We don't check for mainnet/testnet here...
+    return (
+      <Typography component="span" style={style}>
+        {props.variant === "full" || !props.variant
+          ? matchingLocalAccount.name
+          : [
+              matchingLocalAccount.name.substr(0, digits.leading + digits.trailing + 2 - 3).trim(),
+              matchingLocalAccount.name.substr(-3).trim()
+            ].join("…")}
+      </Typography>
+    )
   }
   return (
-    <Typography component="span" style={{ display: "inline", fontSize: "inherit", fontWeight: "bold", ...style }}>
-      {variant === "full" || !variant
-        ? publicKey
-        : publicKey.substr(0, digits.leading) + "…" + publicKey.substr(-digits.trailing)}
+    <Typography component="span" style={style}>
+      {props.variant === "full" || !props.variant
+        ? props.publicKey
+        : props.publicKey.substr(0, digits.leading) + "…" + props.publicKey.substr(-digits.trailing)}
     </Typography>
   )
 }
