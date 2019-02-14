1f981d38667f6b305cebbcfd8a76780dd632451b
Andy Wermke <andy.dev@next-step-software.com>
2019-02-14 11:35:51 +0000

    Add DEX feature flag

--- src/context/settings.tsx
+++ src/context/settings.tsx
@@ -16,17 +16,20 @@ interface Props {
 interface ContextType {
   agreedToTermsAt: string | undefined
   confirmToC: () => void
+  dexTrading: boolean
   ignoreSignatureRequest: (signatureRequestHash: string) => void
   ignoredSignatureRequests: string[]
   multiSignature: boolean
   multiSignatureServiceURL: string
   showTestnet: boolean
+  toggleDexTrading: () => void
   toggleMultiSignature: () => void
   toggleTestnet: () => void
 }
 
 const initialSettings: SettingsData = {
   agreedToTermsAt: undefined,
+  dexTrading: false,
   multisignature: false,
   testnet: false,
   ...loadSettings()
@@ -39,11 +42,13 @@ const multiSignatureServiceURL = process.env.MULTISIG_SERVICE || "https://multis
 const SettingsContext = React.createContext<ContextType>({
   agreedToTermsAt: initialSettings.agreedToTermsAt,
   confirmToC: () => undefined,
+  dexTrading: initialSettings.dexTrading,
   ignoreSignatureRequest: () => undefined,
   ignoredSignatureRequests: initialIgnoredSignatureRequests,
   multiSignature: initialSettings.multisignature,
   multiSignatureServiceURL,
   showTestnet: initialSettings.testnet,
+  toggleDexTrading: () => undefined,
   toggleMultiSignature: () => undefined,
   toggleTestnet: () => undefined
 })
@@ -60,7 +65,7 @@ export function SettingsProvider(props: Props) {
     }
   }
 
-  const updateSettings = (update: Partial<Pick<SettingsData, "agreedToTermsAt" | "multisignature" | "testnet">>) => {
+  const updateSettings = (update: Partial<SettingsData>) => {
     try {
       const updatedSettings = {
         ...settings,
@@ -74,17 +79,20 @@ export function SettingsProvider(props: Props) {
   }
 
   const confirmToC = () => updateSettings({ agreedToTermsAt: new Date().toISOString() })
+  const toggleDexTrading = () => updateSettings({ dexTrading: !settings.dexTrading })
   const toggleMultiSignature = () => updateSettings({ multisignature: !settings.multisignature })
   const toggleTestnet = () => updateSettings({ testnet: !settings.testnet })
 
   const contextValue: ContextType = {
     agreedToTermsAt: settings.agreedToTermsAt,
     confirmToC,
+    dexTrading: settings.dexTrading,
     ignoreSignatureRequest,
     ignoredSignatureRequests,
     multiSignature: settings.multisignature,
     multiSignatureServiceURL,
     showTestnet: settings.testnet,
+    toggleDexTrading,
     toggleMultiSignature,
     toggleTestnet
   }
