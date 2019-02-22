db9f69743052e6181e83da246bb705b065c8e78e
Andy Wermke <andy.dev@next-step-software.com>
2019-02-22 09:19:50 +0000

    Shorten DEX explanation a bit

--- src/components/TradeAsset/Explanation.tsx
+++ src/components/TradeAsset/Explanation.tsx
@@ -5,7 +5,7 @@ import { Box } from "../Layout/Box"
 
 function Explanation() {
   return (
-    <Box alignItems="stretch" justifyContent="stretch" padding={32} style={{ background: grey["100"] }}>
+    <Box alignItems="stretch" grow={1} justifyContent="stretch" padding={32} style={{ background: grey["100"] }}>
       <Typography gutterBottom variant="title">
         Trading assets
       </Typography>
@@ -14,11 +14,11 @@ function Explanation() {
       </Typography>
       <Typography style={{ marginTop: 8 }} variant="body1">
         The trade will be made at the best price currently on offer and should happen immediately. High price volatility
-        and low market volume can potentially make your order spend a prolonged time in the order book.
+        and low market volume can lead to delayed order executions.
       </Typography>
       <Typography style={{ marginTop: 8 }} variant="body1">
-        For this reason you can opt for a higher price limit, increasing your chance of an immediate trade. The Stellar
-        network will always try to buy at the lowest price within your limit first.
+        You can opt for a price tolerance, increasing your chance of an immediate trade. The Stellar network will always
+        try to buy at the lowest price first.
       </Typography>
     </Box>
   )
