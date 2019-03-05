eabba9561c55913075c8d86bc0ffc5045e7a56b9
Andy Wermke <andy.dev@next-step-software.com>
2019-03-05 19:04:25 +0100

    Explicitly use web QR scanner for both electron & web build

--- src/platform/qr-reader.ts
+++ src/platform/qr-reader.ts
@@ -8,7 +8,7 @@ export default function getQRReader(): React.FunctionComponent<Props> {
   if (process.env.PLATFORM === "android" || process.env.PLATFORM === "ios") {
     const QRReader = require("./cordova/qr-reader").default
     return QRReader
-  } else if (process.browser) {
+  } else if (window.electron || process.browser) {
     const QRReader = require("./web/qr-reader").default
     return QRReader
   } else {
