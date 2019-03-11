7cd0ffbfe5c5e554a7b75c680acb65b7ae6850d0
marcelebert <marcel@satoshipay.io>
2019-03-11 17:42:56 +0100

    Add 'MainTitle' component

---
+++ src/components/MainTitle.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import Typography from "@material-ui/core/Typography"
+import { Box, HorizontalLayout } from "./Layout/Box"
+import BackButton from "./BackButton"
+import { PropTypes } from "@material-ui/core"
+
+interface Props {
+  title: React.ReactNode
+  titleColor?: PropTypes.Color
+  style?: React.CSSProperties
+
+  badges?: React.ReactNode
+  actions?: React.ReactNode
+
+  onClose: () => void
+}
+
+const MainTitle = (props: Props) => {
+  return (
+    <HorizontalLayout alignItems="center" margin="0 0 24px" wrap="wrap" style={props.style}>
+      <HorizontalLayout maxWidth="100%">
+        <BackButton onClick={props.onClose} style={{ marginLeft: -8, marginRight: 8 }} />
+        <Typography
+          variant="h5"
+          color={props.titleColor}
+          style={{ overflow: "hidden", maxWidth: "40vw", flexGrow: 1, marginTop: 8, marginRight: 12 }}
+        >
+          {props.title}
+        </Typography>
+
+        {props.badges}
+      </HorizontalLayout>
+
+      <Box grow style={{ textAlign: "right" }}>
+        {props.actions}
+      </Box>
+    </HorizontalLayout>
+  )
+}
+
+export default MainTitle
