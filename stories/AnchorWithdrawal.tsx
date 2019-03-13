ed575c6a3fd16429976dfe7b2c6e224876b26f7a
Andy Wermke <andy.dev@next-step-software.com>
2019-03-13 15:25:30 +0100

    Use storybook action logger for SEP-6 forms

--- stories/AnchorWithdrawal.tsx
+++ stories/AnchorWithdrawal.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { Asset } from "stellar-sdk"
 import { WithdrawalRequestKYC, WithdrawalRequestSuccess } from "@satoshipay/sep-6"
+import { action } from "@storybook/addon-actions"
 import { storiesOf } from "@storybook/react"
 import AnchorWithdrawalFinishForm from "../src/components/Payment/AnchorWithdrawalFinishForm"
 import AnchorWithdrawalInitForm from "../src/components/Payment/AnchorWithdrawalInitForm"
 import AnchorWithdrawalKYCForm from "../src/components/Payment/AnchorWithdrawalKYCForm"
 
-const doNothing = () => undefined
 const eurt = new Asset("EURT", "GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S")
 
 const withdrawalSuccessResponse: WithdrawalRequestSuccess = {
@@ -52,22 +52,27 @@ const withdrawalKYCDeniedResponse: WithdrawalRequestKYC = {
 storiesOf("Withdrawal", module)
   .addDecorator(render => <div style={{ minWidth: "70vw", margin: "20px" }}>{render()}</div>)
   .add("Request", () => (
-    <AnchorWithdrawalInitForm assets={[eurt]} onCancel={doNothing} onSubmit={doNothing} testnet={false} />
+    <AnchorWithdrawalInitForm
+      assets={[eurt]}
+      onCancel={action("clicked cancel")}
+      onSubmit={action("clicked submit")}
+      testnet={false}
+    />
   ))
   .add("Finish", () => (
     <AnchorWithdrawalFinishForm
       anchorResponse={withdrawalSuccessResponse}
-      onCancel={doNothing}
-      onSubmit={doNothing}
+      onCancel={action("cancel")}
+      onSubmit={action("submit")}
       testnet={false}
     />
   ))
   .add("Interactive KYC", () => (
-    <AnchorWithdrawalKYCForm anchorResponse={withdrawalInteractiveKYCResponse} onCancel={doNothing} />
+    <AnchorWithdrawalKYCForm anchorResponse={withdrawalInteractiveKYCResponse} onCancel={action("clicked cancel")} />
   ))
   .add("KYC pending", () => (
-    <AnchorWithdrawalKYCForm anchorResponse={withdrawalKYCPendingResponse} onCancel={doNothing} />
+    <AnchorWithdrawalKYCForm anchorResponse={withdrawalKYCPendingResponse} onCancel={action("clicked cancel")} />
   ))
   .add("KYC denied", () => (
-    <AnchorWithdrawalKYCForm anchorResponse={withdrawalKYCDeniedResponse} onCancel={doNothing} />
+    <AnchorWithdrawalKYCForm anchorResponse={withdrawalKYCDeniedResponse} onCancel={action("clicked cancel")} />
   ))
