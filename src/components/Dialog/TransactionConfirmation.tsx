596eebc0845fe39f801a75cd6ba95107ff6364d2
Marcel-Ebert <marcel@wolfgang-ebert.de>
2019-04-04 13:13:46 +0200

    Use <MainTitle> in TransactionConfirmationDialog

--- src/components/Dialog/TransactionConfirmation.tsx
+++ src/components/Dialog/TransactionConfirmation.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import Dialog from "@material-ui/core/Dialog"
 import Slide from "@material-ui/core/Slide"
-import Typography from "@material-ui/core/Typography"
 import { Operation, Transaction } from "stellar-sdk"
 import { Account } from "../../context/accounts"
 import { SignatureRequest } from "../../lib/multisig-service"
 import ErrorBoundary from "../ErrorBoundary"
 import TxConfirmationForm from "../Form/TxConfirmation"
 import { Box } from "../Layout/Box"
+import MainTitle from "../MainTitle"
 import TestnetBadge from "./TestnetBadge"
 
 const isPaymentOperation = (operation: Operation) => ["createAccount", "payment"].indexOf(operation.type) > -1
@@ -33,12 +33,17 @@ function TxConfirmationDialog(props: TxConfirmationDialogProps) {
       : "Confirm Transaction"
 
   return (
-    <Dialog open={props.open} onClose={props.onClose} maxWidth="lg" TransitionComponent={Transition}>
+    <Dialog open={props.open} fullScreen onClose={props.onClose} maxWidth="lg" TransitionComponent={Transition}>
       <ErrorBoundary>
         <Box padding="24px 36px" overflow="auto">
-          <Typography variant="h5" component="h2">
-            {title} {props.account.testnet ? <TestnetBadge style={{ marginLeft: 8 }} /> : null}
-          </Typography>
+          <MainTitle
+            title={
+              <span>
+                {title} {props.account.testnet ? <TestnetBadge style={{ marginLeft: 8 }} /> : null}
+              </span>
+            }
+            onBack={props.onClose}
+          />
           {props.transaction ? (
             <Box margin="8px 0 0">
               <TxConfirmationForm
