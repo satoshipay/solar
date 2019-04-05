827db9dbd24fab6145161621aafc1adcce83f855
Andy Wermke <andy.dev@next-step-software.com>
2019-04-05 13:21:34 +0200

    Fix stellar address in destination placeholders

--- src/components/Payment/CreatePaymentForm.tsx
+++ src/components/Payment/CreatePaymentForm.tsx
@@ -139,7 +139,7 @@ function PaymentCreationForm(props: Props) {
       <TextField
         error={Boolean(errors.destination)}
         label={errors.destination ? renderFormFieldError(errors.destination) : "Destination address"}
-        placeholder="GABCDEFGHIJK... or alice@example.org"
+        placeholder="GABCDEFGHIJK... or alice*example.org"
         fullWidth
         autoFocus
         margin="normal"
