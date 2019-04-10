0ab24bf9ef86d74a1b2da26569fff3df584e69e4
marcelebert <marcel@satoshipay.io>
2019-04-10 11:33:44 +0200

    Update cordova dependency versions in package.json and config.template

--- cordova/scripts/patch-android-config.sh
+++ cordova/scripts/patch-android-config.sh
@@ -10,5 +10,5 @@ sedi () {
 sedi '/^.*<uses-sdk/d' ../platforms/android/CordovaLib/AndroidManifest.xml
 sedi '/^.*<uses-sdk/d' ../platforms/android/app/src/main/AndroidManifest.xml
 
-# substitute 3.0.2 with 3.3.2 in the line which contains 'com.android.tools.build:gradle'
-sedi '/com.android.tools.build:gradle/s/3.0.1/3.3.2/g' ../platforms/android/build.gradle
+# substitute 3.3.0 with 3.3.2 in the line which contains 'com.android.tools.build:gradle'
+sedi '/com.android.tools.build:gradle/s/3.3.0/3.3.2/g' ../platforms/android/build.gradle
