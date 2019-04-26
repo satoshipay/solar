1895240725343bd5f5bfe9f3ab3e81d963e8fdf9
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-04-26 16:11:41 +0200

    Enable 'userSelect' for <Address> components

--- src/components/PublicKey.tsx
+++ src/components/PublicKey.tsx
@@ -86,8 +86,14 @@ interface AddressProps {
 }
 
 export function Address(props: AddressProps) {
+  const style: React.CSSProperties = {
+    userSelect: "text",
+    WebkitUserSelect: "text",
+    ...props.style
+  }
+
   if (props.address.indexOf("*") > -1) {
-    return <span style={props.style}>{props.address}</span>
+    return <span style={style}>{props.address}</span>
   } else {
     const stellarAddress = queryReverseLookupCache(props.address)
 
@@ -96,7 +102,7 @@ export function Address(props: AddressProps) {
         props.variant === "full" ? stellarAddress : shortenName(stellarAddress, props.variant === "shorter" ? 8 : 14)
 
       return (
-        <span style={props.style}>
+        <span style={style}>
           {formattedStellarAddress} ({<PublicKey publicKey={props.address} variant="shorter" />})
         </span>
       )
