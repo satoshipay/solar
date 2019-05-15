b1b18ae17805c6bef8447ecf7e7d3c9751162f68
Andy Wermke <andy.dev@next-step-software.com>
2019-05-15 16:42:59 +0200

    Quick-fix storybook build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -8,7 +8,9 @@ module.exports = async function customizeWebpackConfig({ config }) {
   config.plugins.push(
     new webpack.NormalModuleReplacementPlugin(/\/platform\/key-store$/, resource => {
       resource.request = resource.request.replace("/platform/key-store", "/platform/web/key-store")
-    })
+    }),
+    // Quick-fix until <https://github.com/stellar/js-stellar-base/pull/196> is merged
+    new webpack.IgnorePlugin(/sodium-native/)
   )
   config.resolve.extensions.push(".ts", ".tsx")
 
