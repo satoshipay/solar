2f2261b962502646355fd65230e267ac8ad38423
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-05-29 10:46:02 +0200

    Add types to SubmissionProgress in storybook

--- stories/SubmissionProgress.tsx
+++ stories/SubmissionProgress.tsx
@@ -3,6 +3,8 @@ import { storiesOf } from "@storybook/react"
 import SubmissionProgress from "../src/components/SubmissionProgress"
 
 storiesOf("SubmissionProgress", module)
-  .add("pending", () => <SubmissionProgress promise={new Promise(resolve => undefined)} />)
-  .add("success", () => <SubmissionProgress promise={Promise.resolve()} />)
-  .add("failed", () => <SubmissionProgress promise={Promise.reject(new Error("Test error"))} />)
+  .add("pending", () => <SubmissionProgress type={"default"} promise={new Promise(resolve => undefined)} />)
+  .add("success", () => <SubmissionProgress type={"default"} promise={Promise.resolve()} />)
+  .add("success-multi-sig", () => <SubmissionProgress type={"multi-sig"} promise={Promise.resolve()} />)
+  .add("success-stellarguard", () => <SubmissionProgress type={"stellarguard"} promise={Promise.resolve()} />)
+  .add("failed", () => <SubmissionProgress type={"default"} promise={Promise.reject(new Error("Test error"))} />)
