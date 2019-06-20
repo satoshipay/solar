edae3bc3e0459bbfc4926722bc87b28bf6d7d1d0
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-06-20 17:16:27 +0200

    Create StellarGuardActivation dialog

---
+++ stories/StellarGuardActivation.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import StellarGuardActivationDialog from "../src/components/Dialog/StellarGuardActivation"
+import { Dialog, Slide } from "@material-ui/core"
+import { Transaction } from "stellar-base"
+import { parseStellarUri, TransactionStellarUri } from "@stellarguard/stellar-uri"
+
+const DialogTransition = (props: any) => <Slide {...props} direction="left" />
+
+const uri = parseStellarUri(
+  "web+stellar:tx?xdr=AAAAADPMT6JWh08TPGnc5nd6eUtw0CfJA4kQjkHZzGEQqGWHAAAAZAAGXSAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAM8xPolaHTxM8adzmd3p5S3DQJ8kDiRCOQdnMYRCoZYcAAAAAAAAAAACYloAAAAAAAAAAAA%3D%3D&msg=order+number+123&callback=url%3Ahttps%3A%2F%2Fexample.com%2Fstellar&origin_domain=test.stellarguard.me&signature=TwoRggPieF6UorVeLHSYZhRRKv8mMwezVUiirms%2F8N6oe8EZOCYKSsNWAn2o1rVb8jhEVte%2FEFZcRkzyXEZdBw%3D%3D"
+)
+
+const accounts = [
+  {
+    id: "testid1",
+    name: "My Testnet Account #1",
+    publicKey: "GBPBFWVBADSESGADWEGC7SGTHE3535FWK4BS6UW3WMHX26PHGIH5NF4W",
+    requiresPassword: false,
+    testnet: true,
+    getPrivateKey: (password: string) => Promise.resolve(password)
+  },
+  {
+    id: "testid2",
+    name: "My Testnet Account #2",
+    publicKey: "GDNVDG37WMKPEIXSJRBAQAVPO5WGOPKZRZZBPLWXULSX6NQNLNQP6CFF",
+    requiresPassword: false,
+    testnet: true,
+    getPrivateKey: (password: string) => Promise.resolve(password)
+  }
+]
+
+const transaction = new Transaction((uri as TransactionStellarUri).xdr)
+
+storiesOf("StellarGuard Activation", module).add("StellarGuard Activation Dialog", () => (
+  <Dialog open={true} fullScreen onClose={undefined} TransitionComponent={DialogTransition}>
+    <StellarGuardActivationDialog accounts={accounts} transaction={transaction} onClose={() => undefined} />
+  </Dialog>
+))
