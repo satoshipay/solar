cedb20af0e3d0bfadc8a5aa8aeea7a9f5b1abee0
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-06-27 16:46:48 +0200

    Add 'babel-polyfill' dependency

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 /// <reference types="parcel-env" />
 
+import "babel-polyfill"
+
 import React from "react"
 import ReactDOM from "react-dom"
 import { HashRouter as Router, Route, Switch } from "react-router-dom"
