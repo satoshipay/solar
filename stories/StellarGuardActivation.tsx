719cf5925f21b3e51bad70577a6a296fc6c4f52b
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-07-05 11:45:25 +0200

    Wrap StellarGuardActivationDialog with Dialog component

--- stories/StellarGuardActivation.tsx
+++ stories/StellarGuardActivation.tsx
@@ -1,15 +1,12 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
-import StellarGuardActivationDialog from "../src/components/Dialog/StellarGuardActivation"
+import { StellarGuardActivationDialog } from "../src/components/Dialog/StellarGuardActivation"
 import { Dialog, Slide } from "@material-ui/core"
 import { Transaction } from "stellar-base"
 import { parseStellarUri, TransactionStellarUri } from "@stellarguard/stellar-uri"
+import { Server } from "stellar-sdk"
 
 const DialogTransition = (props: any) => <Slide {...props} direction="left" />
-
-const uri = parseStellarUri(
-  "web+stellar:tx?xdr=AAAAADPMT6JWh08TPGnc5nd6eUtw0CfJA4kQjkHZzGEQqGWHAAAAZAAGXSAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAM8xPolaHTxM8adzmd3p5S3DQJ8kDiRCOQdnMYRCoZYcAAAAAAAAAAACYloAAAAAAAAAAAA%3D%3D&msg=order+number+123&callback=url%3Ahttps%3A%2F%2Fexample.com%2Fstellar&origin_domain=test.stellarguard.me&signature=TwoRggPieF6UorVeLHSYZhRRKv8mMwezVUiirms%2F8N6oe8EZOCYKSsNWAn2o1rVb8jhEVte%2FEFZcRkzyXEZdBw%3D%3D"
-)
 
 const accounts = [
   {
@@ -30,10 +27,23 @@ const accounts = [
   }
 ]
 
+const horizon = new Server("https://horizon-testnet.stellar.org")
+
+const uri = parseStellarUri(
+  "web+stellar:tx?xdr=AAAAADPMT6JWh08TPGnc5nd6eUtw0CfJA4kQjkHZzGEQqGWHAAAAZAAGXSAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAM8xPolaHTxM8adzmd3p5S3DQJ8kDiRCOQdnMYRCoZYcAAAAAAAAAAACYloAAAAAAAAAAAA%3D%3D&msg=order+number+123&callback=url%3Ahttps%3A%2F%2Fexample.com%2Fstellar&origin_domain=test.stellarguard.me&signature=TwoRggPieF6UorVeLHSYZhRRKv8mMwezVUiirms%2F8N6oe8EZOCYKSsNWAn2o1rVb8jhEVte%2FEFZcRkzyXEZdBw%3D%3D"
+)
+
 const transaction = new Transaction((uri as TransactionStellarUri).xdr)
 
 storiesOf("StellarGuard Activation", module).add("StellarGuard Activation Dialog", () => (
   <Dialog open={true} fullScreen onClose={undefined} TransitionComponent={DialogTransition}>
-    <StellarGuardActivationDialog accounts={accounts} transaction={transaction} onClose={() => undefined} />
+    <StellarGuardActivationDialog
+      sendTransaction={(account, tx) => undefined}
+      accounts={accounts}
+      testnet={true}
+      transaction={transaction}
+      onClose={() => undefined}
+      horizon={horizon}
+    />
   </Dialog>
 ))
