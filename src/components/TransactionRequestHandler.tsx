bbc039d98399afd16d2f86619c0880b74787e31c
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-07-05 11:45:25 +0200

    Wrap StellarGuardActivationDialog with Dialog component

--- src/components/TransactionRequestHandler.tsx
+++ src/components/TransactionRequestHandler.tsx
@@ -1,22 +1,25 @@
 import React from "react"
 import { Transaction } from "stellar-sdk"
-import { TransactionStellarUri } from "@stellarguard/stellar-uri"
+import { StellarUriType, StellarUri, TransactionStellarUri } from "@stellarguard/stellar-uri"
 import StellarGuardActivationDialog from "./Dialog/StellarGuardActivation"
 import { TransactionRequestContext } from "../context/transactionRequest"
+import { Dialog } from "@material-ui/core"
 
-function isStellarGuardTransaction(uri: TransactionStellarUri) {
+function isStellarGuardTransaction(uri: StellarUri) {
   return uri.originDomain === "stellarguard.me" || uri.originDomain === "test.stellarguard.me"
 }
 
 function TransactionRequestHandler() {
   const { uri, clearURI } = React.useContext(TransactionRequestContext)
 
-  const onClose = clearURI
-
-  if (uri instanceof TransactionStellarUri) {
+  if (uri && uri.operation === StellarUriType.Transaction) {
     if (isStellarGuardTransaction(uri)) {
-      const transaction = new Transaction(uri.xdr)
-      return <StellarGuardActivationDialog transaction={transaction} onClose={onClose} />
+      const transaction = new Transaction((uri as TransactionStellarUri).xdr)
+      return (
+        <Dialog open={Boolean(uri)} fullScreen>
+          <StellarGuardActivationDialog testnet={uri.isTestNetwork} transaction={transaction} onClose={clearURI} />
+        </Dialog>
+      )
     } else {
       return <></>
     }
