7950e4b8a5ecbf14b2c427ddd574a11cdc1a6823
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-07-19 16:52:28 +0200

    Remove wellKnownStellarAccounts json and ts file

--- src/lib/wellKnownStellarAccounts.ts
+++
@@ -1,9 +0,0 @@
-import wellKnownAccountsData from "../well-known-stellar-accounts.json"
-
-const wellKnownAccounts = wellKnownAccountsData._embedded.records
-
-export function isKnownExchange(publicKey: string) {
-  return Boolean(
-    wellKnownAccounts.find(account => account.address === publicKey && account.tags.indexOf("exchange") !== -1)
-  )
-}
