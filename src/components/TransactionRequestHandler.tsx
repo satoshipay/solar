1b06d02343df4c0e57ee22e3cd0ab62dff6cf962
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-08-21 15:31:19 +0200

    Use SolarUri in transaction request context and handler

--- src/components/TransactionRequestHandler.tsx
+++ src/components/TransactionRequestHandler.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import { Transaction } from "stellar-sdk"
 import Fade from "@material-ui/core/Fade"
 import { TransitionProps } from "@material-ui/core/transitions/transition"
+import { Dialog } from "@material-ui/core"
 import { StellarUriType, StellarUri, TransactionStellarUri } from "@stellarguard/stellar-uri"
 import StellarGuardActivationDialog from "./Dialog/StellarGuardActivation"
 import { TransactionRequestContext } from "../context/transactionRequest"
-import { Dialog } from "@material-ui/core"
+import { SolarUriType, SolarUri } from "../lib/solar-uri"
+import CreateAccountPage from "../pages/create-account"
 
 const Transition = React.forwardRef((props: TransitionProps, ref) => <Fade ref={ref} {...props} />)
 
@@ -15,7 +17,7 @@ function isStellarGuardTransaction(uri: StellarUri) {
 
 function TransactionRequestHandler() {
   const { uri, clearURI } = React.useContext(TransactionRequestContext)
-  const [closedStellarURI, setClosedStellarURI] = React.useState<StellarUri | null>(null)
+  const [closedStellarURI, setClosedStellarURI] = React.useState<StellarUri | SolarUri | null>(null)
 
   // We need that so we still know what to render when we fade out the dialog
   const renderedURI = uri || closedStellarURI
@@ -24,11 +26,18 @@ function TransactionRequestHandler() {
     () => {
       setClosedStellarURI(uri)
       clearURI()
+
+      // Clear location href, since it might contain secret search params
+      window.history.pushState({}, "Solar Wallet", window.location.href.replace(window.location.search, ""))
     },
     [uri]
   )
 
-  if (renderedURI && renderedURI.operation === StellarUriType.Transaction) {
+  if (!renderedURI) {
+    return null
+  }
+
+  if (renderedURI.operation === StellarUriType.Transaction) {
     if (isStellarGuardTransaction(renderedURI)) {
       const transaction = new Transaction((renderedURI as TransactionStellarUri).xdr)
       return (
@@ -41,7 +50,17 @@ function TransactionRequestHandler() {
         </Dialog>
       )
     }
+  } else if (renderedURI.operation === SolarUriType.Import) {
+    const secretKey = renderedURI.getParam("secret")
+    if (secretKey) {
+      return (
+        <Dialog open={Boolean(uri)} fullScreen TransitionComponent={Transition}>
+          <CreateAccountPage importedSecretKey={secretKey} onClose={closeDialog} testnet={renderedURI.isTestNetwork} />
+        </Dialog>
+      )
+    }
   }
+
   return null
 }
 
--- src/context/transactionRequest.tsx
+++ src/context/transactionRequest.tsx
@@ -1,8 +1,9 @@
 import React from "react"
-import { StellarUri } from "@stellarguard/stellar-uri"
+import { StellarUri, StellarUriType } from "@stellarguard/stellar-uri"
 import { verifyTransactionRequest } from "../lib/stellar-uri"
 import { subscribeToDeepLinkURLs } from "../platform/protocol-handler"
 import { trackError } from "./notifications"
+import { SolarUri, SolarUriType } from "../lib/solar-uri"
 
 const allowUnsafeTestnetURIs = Boolean(process.env.ALLOW_UNSAFE_TESTNET_URIS)
 
@@ -11,7 +12,7 @@ interface Props {
 }
 
 interface ContextType {
-  uri: StellarUri | null
+  uri: StellarUri | SolarUri | null
   clearURI: () => void
 }
 
@@ -23,17 +24,37 @@ const initialValues: ContextType = {
 const TransactionRequestContext = React.createContext<ContextType>(initialValues)
 
 export function TransactionRequestProvider(props: Props) {
-  const [uri, setURI] = React.useState<StellarUri | null>(null)
+  const [uri, setURI] = React.useState<StellarUri | SolarUri | null>(null)
 
   const clearURI = React.useCallback(() => setURI(null), [])
 
+  const verifyStellarURI = React.useCallback(async (incomingURI: string) => {
+    try {
+      const parsedURI = await verifyTransactionRequest(incomingURI, { allowUnsafeTestnetURIs })
+      setURI(parsedURI)
+    } catch (error) {
+      trackError(error)
+    }
+  }, [])
+
+  const verifySolarURI = (incomingURI: string) => {
+    const solarUri = new SolarUri(incomingURI)
+    setURI(solarUri)
+  }
+
   React.useEffect(() => {
     const unsubscribe = subscribeToDeepLinkURLs(async incomingURI => {
-      try {
-        const parsedURI = await verifyTransactionRequest(incomingURI, { allowUnsafeTestnetURIs })
-        setURI(parsedURI)
-      } catch (error) {
-        trackError(error)
+      const url = new URL(incomingURI)
+      switch (url.pathname) {
+        case StellarUriType.Transaction || StellarUriType.Pay:
+          verifyStellarURI(incomingURI)
+          break
+        case SolarUriType.Import:
+          verifySolarURI(incomingURI)
+          break
+        default:
+          trackError(new Error(`Incoming uri ${incomingURI} does not match any expected type.`))
+          break
       }
     })
     return unsubscribe
