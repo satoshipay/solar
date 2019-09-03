d4aa9dff073b0e8937a721982232e568132f020a
Andy Wermke <andy.dev@next-step-software.com>
2019-09-03 17:25:07 +0200

    Add useDebouncedState() for a small performance improvement

--- src/hooks.ts
+++ src/hooks.ts
@@ -47,11 +47,54 @@ export function useHorizon(testnet: boolean = false) {
   return testnet ? stellar.horizonTestnet : stellar.horizonLivenet
 }
 
+export function useDebouncedState<T>(initial: T, delay: number = 50): [T, (update: T | ((prev: T) => T)) => void] {
+  const currentCallGroupTimeoutRef = React.useRef<any>(undefined)
+  const updateQueueRef = React.useRef<Array<T | ((prev: T) => T)> | undefined>(undefined)
+  const [state, setState] = React.useState(initial)
+
+  const applyUpdateQueue = (previous: T, queue: Array<T | ((prev: T) => T)>) => {
+    return queue.reduce<T>(
+      (intermediate, queuedUpdate) =>
+        typeof queuedUpdate === "function" ? (queuedUpdate as ((p: T) => T))(intermediate) : queuedUpdate,
+      previous
+    )
+  }
+
+  const debouncedSetState = React.useCallback((update: T | ((prev: T) => T)) => {
+    if (currentCallGroupTimeoutRef.current) {
+      updateQueueRef.current!.push(update)
+    } else {
+      currentCallGroupTimeoutRef.current = setTimeout(() => {
+        if (updateQueueRef.current) {
+          const queue = updateQueueRef.current
+          setState(prev => applyUpdateQueue(prev, queue))
+        }
+        currentCallGroupTimeoutRef.current = undefined
+        updateQueueRef.current = undefined
+      }, delay)
+      updateQueueRef.current = []
+      setState(update)
+    }
+  }, [])
+
+  React.useEffect(() => {
+    const onUnmount = () => {
+      if (currentCallGroupTimeoutRef.current) {
+        clearTimeout(currentCallGroupTimeoutRef.current)
+      }
+    }
+    return onUnmount
+  }, [])
+
+  return [state, debouncedSetState]
+}
+
 // TODO: Better to separate fetch() & subscribeToUpdates(), have two useEffects()
 
 function useDataSubscriptions<ObservedData>(subscriptions: Array<SubscriptionTarget<ObservedData>>): ObservedData[] {
-  const [currentDataSets, setCurrentDataSets] = React.useState<ObservedData[]>(
-    subscriptions.map(subscription => subscription.getLatest())
+  const [currentDataSets, setCurrentDataSets] = useDebouncedState<ObservedData[]>(
+    subscriptions.map(subscription => subscription.getLatest()),
+    100
   )
 
   const updateDataSets = (
