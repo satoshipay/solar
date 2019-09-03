cfac62e5ba6a1932a5d6d2d2ca3512568abdce33
Andy Wermke <andy.dev@next-step-software.com>
2019-09-03 11:47:32 +0200

    Small settings page and toggle section adjustments

--- src/components/Layout/ToggleSection.tsx
+++ src/components/Layout/ToggleSection.tsx
@@ -1,7 +1,44 @@
 import React from "react"
 import Switch from "@material-ui/core/Switch"
 import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/core/styles"
 import { Box } from "./Box"
+
+const useToggleSectionStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 24,
+
+    "&:first-child": {
+      marginTop: 0
+    }
+  },
+  row: {
+    display: "flex",
+    flexWrap: "nowrap"
+  },
+  left: {
+    flexShrink: 0,
+    marginLeft: -12,
+    width: 70
+  },
+  right: {
+    flexGrow: 1,
+    flexShrink: 0,
+    width: "calc(100% - 70px)"
+  },
+  heading: {
+    display: "flex",
+    alignItems: "center",
+    fontSize: 18,
+    height: 48,
+    lineHeight: "24px"
+  },
+  headingSpan: {
+    cursor: "pointer"
+  }
+})
 
 interface Props {
   checked: boolean
@@ -13,26 +50,28 @@ interface Props {
 }
 
 function ToggleSection(props: Props) {
-  const onChange = () => {
-    if (props.onChange && !props.disabled) {
-      props.onChange()
-    }
-  }
+  const classes = useToggleSectionStyles()
+  const onChange = props.onChange && !props.disabled ? props.onChange : undefined
   return (
-    <Box style={{ display: "flex", margin: "24px 0 0", ...props.style }}>
-      <Box margin="6px 0 0" width={70} shrink={0}>
-        <Switch color="primary" checked={props.checked} disabled={props.disabled} onChange={props.onChange} />
+    <Box className={classes.root} style={props.style}>
+      <Box className={classes.row} alignItems="center">
+        <Box className={classes.left}>
+          <Switch color="primary" checked={props.checked} disabled={props.disabled} onChange={props.onChange} />
+        </Box>
+        <Box className={classes.right}>
+          <Typography className={classes.heading} variant="h6">
+            <span className={classes.headingSpan} onClick={onChange}>
+              {props.title}
+            </span>
+          </Typography>
+        </Box>
       </Box>
-      <Box grow shrink={0} width="calc(100% - 70px)">
-        <Typography variant="h6" style={{ display: "flex", alignItems: "center", height: 48 }}>
-          <span onClick={onChange} style={{ cursor: "pointer" }}>
-            {props.title}
-          </span>
-        </Typography>
-        {props.children}
+      <Box className={classes.row}>
+        <Box className={classes.left}>{null}</Box>
+        <Box className={classes.right}>{props.children}</Box>
       </Box>
     </Box>
   )
 }
 
-export default ToggleSection
+export default React.memo(ToggleSection)
