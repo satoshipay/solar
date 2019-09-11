e4f6144e63cfebd281b68887b3d859486aafed80
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-09-11 17:58:53 +0200

    Add flag that indicates the availability of biometric authentication to settings context

--- src/context/settings.tsx
+++ src/context/settings.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { testBiometricAuth } from "../platform/cordova/bio-auth"
+import { testBiometricAuth, isBiometricAuthAvailable } from "../platform/cordova/bio-auth"
 import {
   loadIgnoredSignatureRequestHashes,
   loadSettings,
@@ -16,6 +16,7 @@ interface Props {
 interface ContextType {
   agreedToTermsAt: string | undefined
   biometricLock: boolean
+  biometricLockUsable: boolean
   confirmToC: () => void
   ignoreSignatureRequest: (signatureRequestHash: string) => void
   ignoredSignatureRequests: string[]
@@ -44,6 +45,7 @@ const multiSignatureServiceURL = process.env.MULTISIG_SERVICE || "https://multis
 const SettingsContext = React.createContext<ContextType>({
   agreedToTermsAt: initialSettings.agreedToTermsAt,
   biometricLock: initialSettings.biometricLock,
+  biometricLockUsable: false,
   confirmToC: () => undefined,
   ignoreSignatureRequest: () => undefined,
   ignoredSignatureRequests: initialIgnoredSignatureRequests,
@@ -60,6 +62,7 @@ const SettingsContext = React.createContext<ContextType>({
 export function SettingsProvider(props: Props) {
   const [ignoredSignatureRequests, setIgnoredSignatureRequests] = React.useState(initialIgnoredSignatureRequests)
   const [settings, setSettings] = React.useState(initialSettings)
+  const [biometricLockUsable, setBiometricLockUsable] = React.useState(false)
 
   React.useEffect(() => {
     Promise.all([loadIgnoredSignatureRequestHashes(), loadSettings()])
@@ -68,6 +71,10 @@ export function SettingsProvider(props: Props) {
         setSettings({ ...settings, ...loadedSettings })
       })
       .catch(trackError)
+
+    isBiometricAuthAvailable().then(available => {
+      setBiometricLockUsable(available)
+    })
 
     // Can't really cancel loading the settings
     const unsubscribe = () => undefined
@@ -113,6 +120,7 @@ export function SettingsProvider(props: Props) {
   const contextValue: ContextType = {
     agreedToTermsAt: settings.agreedToTermsAt,
     biometricLock: settings.biometricLock,
+    biometricLockUsable,
     confirmToC,
     ignoreSignatureRequest,
     ignoredSignatureRequests,
