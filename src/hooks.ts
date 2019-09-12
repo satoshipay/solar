77b4c9fb96a9e2dd90f21ce19a23902fbcfb6c48
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-09-12 11:54:12 +0200

    Revert unnecessary code reformatting

--- src/hooks.ts
+++ src/hooks.ts
@@ -108,17 +108,22 @@ function useDataSubscriptions<ObservedData>(subscriptions: Array<SubscriptionTar
 
   // Asynchronously subscribe to remote data to keep state in sync
   // `unsubscribe` will only unsubscribe state updating code, won't close remote data subscription itself
-  React.useEffect(() => {
-    // Some time has passed since the last `getLatest()`, so refresh
-    setCurrentDataSets(subscriptions.map(subscription => subscription.getLatest()))
+  React.useEffect(
+    () => {
+      // Some time has passed since the last `getLatest()`, so refresh
+      setCurrentDataSets(subscriptions.map(subscription => subscription.getLatest()))
 
-    const unsubscribeHandlers = subscriptions.map((subscription, index) =>
-      subscription.subscribe(update => setCurrentDataSets(prevDataSets => updateDataSets(prevDataSets, update, index)))
-    )
+      const unsubscribeHandlers = subscriptions.map((subscription, index) =>
+        subscription.subscribe(update =>
+          setCurrentDataSets(prevDataSets => updateDataSets(prevDataSets, update, index))
+        )
+      )
 
-    const unsubscribe = () => unsubscribeHandlers.forEach(unsubscribeHandler => unsubscribeHandler())
-    return unsubscribe
-  }, [subscriptions])
+      const unsubscribe = () => unsubscribeHandlers.forEach(unsubscribeHandler => unsubscribeHandler())
+      return unsubscribe
+    },
+    [subscriptions]
+  )
 
   return currentDataSets
 }
@@ -156,16 +161,19 @@ export function useAccountEffectSubscriptions(accounts: Account[], handler: Effe
   const mainnetHorizon = useHorizon(false)
   const testnetHorizon = useHorizon(true)
 
-  return React.useEffect(() => {
-    const unsubscribeHandlers = accounts.map(account => {
-      const horizon = account.testnet ? testnetHorizon : mainnetHorizon
-      const subscription = subscribeToAccountEffects(horizon, account.publicKey)
-      const unsubscribe = subscription.subscribe(effect => effect && handler(account, effect))
-      return unsubscribe
-    })
+  return React.useEffect(
+    () => {
+      const unsubscribeHandlers = accounts.map(account => {
+        const horizon = account.testnet ? testnetHorizon : mainnetHorizon
+        const subscription = subscribeToAccountEffects(horizon, account.publicKey)
+        const unsubscribe = subscription.subscribe(effect => effect && handler(account, effect))
+        return unsubscribe
+      })
 
-    return () => unsubscribeHandlers.forEach(unsubscribe => unsubscribe())
-  }, [accounts, mainnetHorizon, testnetHorizon])
+      return () => unsubscribeHandlers.forEach(unsubscribe => unsubscribe())
+    },
+    [accounts, mainnetHorizon, testnetHorizon]
+  )
 }
 
 export function useClipboard() {
@@ -291,27 +299,30 @@ export function useStellarTomlFiles(domains: string[]): Map<string, [StellarToml
   const loadingStates = React.useContext(StellarTomlLoadingCacheContext)
   const resultMap = new Map<string, [StellarToml, boolean]>()
 
-  React.useEffect(() => {
-    for (const domain of domains) {
-      // This is semantically different from `.filter()`-ing above, since this will
-      // prevent double-fetching from domains that were part of this iteration
-      if (stellarTomls.cache.has(domain) || loadingStates.cache.has(domain)) {
-        continue
+  React.useEffect(
+    () => {
+      for (const domain of domains) {
+        // This is semantically different from `.filter()`-ing above, since this will
+        // prevent double-fetching from domains that were part of this iteration
+        if (stellarTomls.cache.has(domain) || loadingStates.cache.has(domain)) {
+          continue
+        }
+
+        loadingStates.store(domain, AsyncStatus.pending())
+
+        StellarTomlResolver.resolve(domain)
+          .then(stellarTomlData => {
+            loadingStates.delete(domain)
+            stellarTomls.store(domain, stellarTomlData)
+            localStorage.setItem(createStellarTomlCacheKey(domain), JSON.stringify(stellarTomlData))
+          })
+          .catch(error => {
+            loadingStates.store(domain, AsyncStatus.rejected(error))
+          })
       }
-
-      loadingStates.store(domain, AsyncStatus.pending())
-
-      StellarTomlResolver.resolve(domain)
-        .then(stellarTomlData => {
-          loadingStates.delete(domain)
-          stellarTomls.store(domain, stellarTomlData)
-          localStorage.setItem(createStellarTomlCacheKey(domain), JSON.stringify(stellarTomlData))
-        })
-        .catch(error => {
-          loadingStates.store(domain, AsyncStatus.rejected(error))
-        })
-    }
-  }, [domains, loadingStates, stellarTomls])
+    },
+    [domains, loadingStates, stellarTomls]
+  )
 
   for (const domain of domains) {
     const cached = stellarTomls.cache.get(domain)
@@ -380,10 +391,13 @@ export function useRouter<Params = {}>() {
     throw new Error("useRouter() hook can only be used within a react-router provider.")
   }
 
-  React.useEffect(() => {
-    const unsubscribe = routerContext.history.listen(() => forceUpdate())
-    return unsubscribe
-  }, [routerContext])
+  React.useEffect(
+    () => {
+      const unsubscribe = routerContext.history.listen(() => forceUpdate())
+      return unsubscribe
+    },
+    [routerContext]
+  )
 
   return routerContext
 }
