satoshipay/solar

commit 49b27e2f8d1b5179ce78072e456d61549db74e93

Fix no error shown on wrong password (#768)
* Rename WrongPasswordError

* Propagate WrongPasswordErrors if key data decryption with password fails

electron/lib/storage.js
@@ -71,7 +71,12 @@ expose(ipcMain, commands.signTransactionCommand, events.signTransactionEvent, fu
   password
 ) {
   const transaction = new Transaction(txEnvelopeXdr, networkPassphrase)
-  const { privateKey } = keystore.getPrivateKeyData(keyID, password)
+  let privateKey
+  try {
+    privateKey = keystore.getPrivateKeyData(keyID, password)
+  } catch (error) {
+    throw Object.assign(new Error("Wrong password."), { name: "WrongPasswordError" })
+  }
 
   transaction.sign(Keypair.fromSecret(privateKey))
 

src/context/accounts.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Keypair } from "stellar-sdk"
 import { Transaction } from "stellar-base"
-import { createWrongPasswordError } from "../lib/errors"
+import { WrongPasswordError } from "../lib/errors"
 import getKeyStore from "../platform/key-store"
 import { KeyStoreAPI } from "../platform/types"
 import { trackError } from "./notifications"
@@ -62,7 +62,7 @@ async function createAccountInstance(keyStore: KeyStoreAPI, keyID: string) {
       } catch (error) {
         // tslint:disable-next-line:no-console
         console.debug("Decrypting private key data failed. Assuming wrong password:", error)
-        throw createWrongPasswordError()
+        throw WrongPasswordError()
       }
     },
 
@@ -192,7 +192,7 @@ export function AccountsProvider(props: Props) {
     } catch (error) {
       // tslint:disable-next-line:no-console
       console.debug("Decrypting private key data failed. Assuming wrong password:", error)
-      throw createWrongPasswordError()
+      throw WrongPasswordError()
     }
 
     // Setting `password: true` explicitly, in case there was no password set before
@@ -212,7 +212,7 @@ export function AccountsProvider(props: Props) {
     } catch (error) {
       // tslint:disable-next-line:no-console
       console.debug("Decrypting private key data failed. Assuming wrong password:", error)
-      throw createWrongPasswordError()
+      throw WrongPasswordError()
     }
 
     await keyStore.saveKey(accountID, "", privateKeyData, { ...publicKeyData, password: false })

src/cordova/keystore.ts
@@ -119,10 +119,18 @@ async function respondWithSignedTransaction(
   const { keyID, networkPassphrase, password, transactionEnvelope } = event.data
 
   const transaction = new Transaction(transactionEnvelope, networkPassphrase)
-  const { privateKey } = keyStore.getPrivateKeyData(keyID, password)
+
+  let privateKey: string
+  try {
+    privateKey = keyStore.getPrivateKeyData(keyID, password).privateKey
+  } catch (error) {
+    // tslint:disable-next-line:no-console
+    console.debug("Decrypting private key data failed. Assuming wrong password:", error)
+    contentWindow.postMessage({ eventType: events.removeKeyEvent, id: event.data.id, error: "Wrong password" }, "*")
+    return
+  }
 
   transaction.sign(Keypair.fromSecret(privateKey))
-
   const result = transaction.toEnvelope().toXDR("base64")
 
   contentWindow.postMessage({ eventType: events.removeKeyEvent, id: event.data.id, result }, "*")

src/lib/errors.ts
@@ -10,7 +10,7 @@ export function isCancellation(thing: any) {
   return thing instanceof Error && thing.name === "Cancellation"
 }
 
-export function createWrongPasswordError(message: string = "Wrong password.") {
+export function WrongPasswordError(message: string = "Wrong password.") {
   return Object.assign(new Error(message), { name: "WrongPasswordError" })
 }
 

src/lib/transaction.ts
@@ -11,7 +11,7 @@ import {
   Networks
 } from "stellar-sdk"
 import { Account } from "../context/accounts"
-import { createWrongPasswordError } from "../lib/errors"
+import { WrongPasswordError } from "../lib/errors"
 import { getAllSources, isNotFoundError, isSignedByAnyOf, selectSmartTransactionFee, SmartFeePreset } from "./stellar"
 
 interface SignatureWithHint extends xdr.DecoratedSignature {
@@ -146,7 +146,7 @@ export async function createPaymentOperation(options: PaymentOperationBlueprint)
 
 export async function signTransaction(transaction: Transaction, walletAccount: Account, password: string | null) {
   if (walletAccount.requiresPassword && !password) {
-    throw createWrongPasswordError(`Account is password-protected, but no password has been provided.`)
+    throw WrongPasswordError(`Account is password-protected, but no password has been provided.`)
   }
 
   const signedTransaction = walletAccount.signTransaction(transaction, password)

src/platform/cordova/key-store.ts
@@ -4,6 +4,7 @@ import { Account } from "../../context/accounts"
 import { networkPassphrases } from "../../lib/stellar"
 import { KeyStoreAPI } from "../types"
 import { sendCommand } from "./message-handler"
+import { WrongPasswordError } from "../../lib/errors"
 
 export default async function createKeyStore(): Promise<KeyStoreAPI> {
   return {
@@ -37,9 +38,13 @@ export default async function createKeyStore(): Promise<KeyStoreAPI> {
         password,
         transactionEnvelope
       })
-      const signedTransactionEnvelope = event.data.result
-      const networkPassphrase = account.testnet ? Networks.TESTNET : Networks.PUBLIC
-      return new Transaction(signedTransactionEnvelope, networkPassphrase)
+      if (event.data.error === "Wrong password") {
+        throw WrongPasswordError()
+      } else {
+        const signedTransactionEnvelope = event.data.result
+        const networkPassphrase = account.testnet ? Networks.TESTNET : Networks.PUBLIC
+        return new Transaction(signedTransactionEnvelope, networkPassphrase)
+      }
     },
     async removeKey(keyID: string) {
       const data = { keyID }

src/platform/web/key-store.ts
@@ -2,6 +2,7 @@ import { createStore, KeysData } from "key-store"
 import { Keypair, Transaction, Networks } from "stellar-sdk"
 import { Account } from "../../context/accounts"
 import { KeyStoreAPI } from "../types"
+import { WrongPasswordError } from "../../lib/errors"
 
 const defaultTestingKeys: KeysData<PublicKeyData> = {
   "1": {
@@ -66,7 +67,13 @@ export default async function createKeyStore(): Promise<KeyStoreAPI> {
     saveKey: async (...args) => keyStore.saveKey(...args),
     savePublicKeyData: async (...args) => keyStore.savePublicKeyData(...args),
     async signTransaction(tx: Transaction, account: Account, password: string): Promise<Transaction> {
-      const { privateKey } = keyStore.getPrivateKeyData(account.id, password)
+      let privateKey: string
+      try {
+        privateKey = keyStore.getPrivateKeyData(account.id, password).privateKey
+      } catch (error) {
+        throw WrongPasswordError()
+      }
+
       const keypair = Keypair.fromSecret(privateKey)
 
       const networkPassphrase = account.testnet ? Networks.TESTNET : Networks.PUBLIC

commit 492f173eedbf3a86b292fd95f1b1bef07cb429cb

Fix error introduced in #768

electron/lib/storage.js
@@ -73,7 +73,7 @@ expose(ipcMain, commands.signTransactionCommand, events.signTransactionEvent, fu
   const transaction = new Transaction(txEnvelopeXdr, networkPassphrase)
   let privateKey
   try {
-    privateKey = keystore.getPrivateKeyData(keyID, password)
+    privateKey = keystore.getPrivateKeyData(keyID, password).privateKey
   } catch (error) {
     throw Object.assign(new Error("Wrong password."), { name: "WrongPasswordError" })
   }