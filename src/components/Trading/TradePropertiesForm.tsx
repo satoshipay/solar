5f55d62d9070511b80a4e45f0a88614558a8f566
marcel <marcel@wolfgang-ebert.de>
2019-10-11 14:23:28 +0200

    Prevent division by zero at calculation of default price

--- src/components/Trading/TradePropertiesForm.tsx
+++ src/components/Trading/TradePropertiesForm.tsx
@@ -98,7 +98,8 @@ function TradePropertiesForm(props: TradePropertiesFormProps) {
   const [manualPrice, setManualPrice] = React.useState<ManualPrice>({})
   const [priceMode, setPriceMode] = React.useState<"fixed-buying" | "fixed-selling">("fixed-selling")
 
-  const defaultPrice = bigNumberToInputValue(priceMode === "fixed-buying" ? BigNumber(1).div(props.price) : props.price)
+  const price = props.price.eq(0) ? BigNumber(1) : props.price // prevent division by zero
+  const defaultPrice = bigNumberToInputValue(priceMode === "fixed-buying" ? BigNumber(1).div(price) : price)
 
   const buyingAmountString =
     editableAmount.field === "buying" ? editableAmount.value : bigNumberToInputValue(props.buyingAmount)
