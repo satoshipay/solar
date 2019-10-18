35b28dfb9e2633e6a4f35cc4747e4716b2ba504c
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-10-18 16:52:31 +0200

    Move electron types from 'src/types' to 'electron/types' directory

--- electron/types/types.d.ts
+++ electron/types/types.d.ts
@@ -1,3 +1,38 @@
+interface SettingsData {
+  agreedToTermsAt?: string
+  multisignature: boolean
+  testnet: boolean
+}
+
+interface ElectronContext {
+  getKeyIDs(): Promise<string[]>
+  getPublicKeyData(keyID: string): Promise<PublicKeyData>
+  getPrivateKeyData(keyID: string, password: string): Promise<PrivateKeyData>
+  saveKey(keyID: string, password: string, privateData: PrivateKeyData, publicData?: PublicKeyData): Promise<void>
+  savePublicKeyData(keyID: string, publicData: PublicKeyData): Promise<void>
+  signTransaction(txEnvelopeXdr: string, keyID: string, networkPassphrase: string, password: string): Promise<string>
+  removeKey(keyID: string): Promise<void>
+
+  readIgnoredSignatureRequestHashes(): string[]
+  readSettings(): SettingsData
+  updateIgnoredSignatureRequestHashes(updatedHashes: string[]): void
+  updateSettings(updatedSettings: Partial<SettingsData>): void
+  subscribeToIPCMain(channel: string, subscribeCallback: (event: Event, ...args: any[]) => void): () => void
+}
+
+interface Window {
+  // Will only be defined when in an electron build
+  electron?: ElectronContext
+}
+
+declare module NodeJS {
+  interface Global {
+    // Will only be defined when in an electron build
+    electron?: ElectronContext
+    process: NodeJS.Process
+  }
+}
+
 declare module "electron-reload" {
   export default function autoReload(
     paths: string,
