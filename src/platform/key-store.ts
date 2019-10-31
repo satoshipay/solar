655284bda03496b1503e555d66a1104533489709
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-10-31 09:30:12 +0100

    Use ipc message calls in key-store

--- src/platform/key-store.ts
+++ src/platform/key-store.ts
@@ -1,16 +1,34 @@
-import { KeyStoreAPI } from "./types"
+import { call } from "./ipc"
+import { Transaction } from "stellar-sdk"
 
-export default function getKeyStore(): Promise<KeyStoreAPI> {
-  if (window.electron) {
-    const createElectronKeyStore = require("./electron/key-store").default
-    return createElectronKeyStore()
-  } else if (process.env.PLATFORM === "android" || process.env.PLATFORM === "ios") {
-    const createCordovaKeyStore = require("./cordova/key-store").default
-    return createCordovaKeyStore()
-  } else if (process.browser) {
-    const createBrowserKeyStore = require("./web/key-store").default
-    return createBrowserKeyStore()
-  } else {
-    throw new Error("There is no implementation for your platform.")
-  }
+export interface KeyStoreAPI {
+  getKeyIDs(): Promise<string[]>
+  getPublicKeyData(keyID: string): Promise<PublicKeyData>
+  getPrivateKeyData(keyID: string, password: string): Promise<PrivateKeyData>
+  saveKey(keyID: string, password: string, privateData: PrivateKeyData, publicData?: PublicKeyData): Promise<void>
+  savePublicKeyData(keyID: string, publicData: PublicKeyData): Promise<void>
+  signTransaction(internalAccountID: string, transaction: Transaction, password: string): Promise<Transaction>
+  removeKey(keyID: string): Promise<void>
+}
+
+const keyStore: KeyStoreAPI = {
+  getKeyIDs: () => call(IPC.Messages.GetKeyIDs),
+  getPublicKeyData: keyID => call(IPC.Messages.GetPublicKeyData, keyID),
+  getPrivateKeyData: (keyID, password) => call(IPC.Messages.GetPrivateKeyData, keyID, password),
+  signTransaction: async (accountID, transaction, password) => {
+    const txXDR = transaction
+      .toEnvelope()
+      .toXDR("base64")
+      .toString("base64")
+    const signedXDR = await call(IPC.Messages.SignTransaction, accountID, txXDR, password)
+    return new Transaction(signedXDR)
+  },
+  saveKey: (keyID, password, privateData, publicData) =>
+    call(IPC.Messages.SaveKey, keyID, password, privateData, publicData),
+  savePublicKeyData: (keyID, publicData) => call(IPC.Messages.SavePublicKeyData, keyID, publicData),
+  removeKey: keyID => call(IPC.Messages.RemoveKey, keyID)
+}
+
+export default function getKeyStore(): KeyStoreAPI {
+  return keyStore
 }
