90c56ce5460076c086f10031bb48197d8c7cddab
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-10-31 09:37:06 +0100

    Use ipc in clipboard and links implementation

--- src/platform/clipboard.ts
+++ src/platform/clipboard.ts
@@ -1,19 +1,5 @@
-interface ClipboardFunctions {
-  copyToClipboard(text: string): Promise<any>
-}
-
-const clipboard = getClipboardFunctions()
-
-function getClipboardFunctions(): ClipboardFunctions {
-  if (process.env.PLATFORM === "android" || process.env.PLATFORM === "ios") {
-    return require("./cordova/clipboard")
-  } else if (window.electron || process.browser) {
-    return require("./web/clipboard")
-  } else {
-    throw new Error("There is no implementation for your platform.")
-  }
-}
+import { call } from "./ipc"
 
 export function copyToClipboard(text: string) {
-  return clipboard.copyToClipboard(text)
+  return call(IPC.Messages.CopyToClipboard, text)
 }
--- src/platform/links.ts
+++ src/platform/links.ts
@@ -1,19 +1,5 @@
-interface LinkFunctions {
-  openLink(href: string): void
-}
-
-const Weblinks = getLinkFunctions()
-
-function getLinkFunctions(): LinkFunctions {
-  if (process.env.PLATFORM === "android" || process.env.PLATFORM === "ios") {
-    return require("./cordova/links")
-  } else if (window.electron || process.browser) {
-    return require("./web/links")
-  } else {
-    throw new Error("There is no implementation for your platform.")
-  }
-}
+import { call } from "./ipc"
 
 export function openLink(href: string) {
-  return Weblinks.openLink(href)
+  return call(IPC.Messages.OpenLink, href)
 }
