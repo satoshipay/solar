aa7e83a18e0b05a23f7efab8a76b6c1df9994fe7
Andy Wermke <github@andywer.com>
2019-11-18 11:24:16 +0100

    Upgrade to parcel v2

--- src/worker-controller.ts
+++ src/worker-controller.ts
@@ -3,7 +3,9 @@ import { spawn } from "threads"
 import { NetWorker } from "./workers/net-worker/worker"
 
 async function spawnWorkers() {
-  const netWorker = await spawn<NetWorker>(new Worker("./workers/net-worker/worker.ts"))
+  const netWorker = await spawn<NetWorker>(
+    new Worker("./workers/net-worker/worker.ts", { name: "net-worker", type: "classic" })
+  )
 
   return {
     netWorker
