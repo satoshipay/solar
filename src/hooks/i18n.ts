1edee1824bde7a141b004eb5b1ecccd1bd81108e
Marcel Ebert <mail@marcel-ebert.de>
2020-01-09 18:01:52 +0100

    Create 'useTranslation'-hook for i18n

---
+++ src/hooks/i18n.ts
@@ -0,0 +1,51 @@
+import React from "react"
+
+const translations = {
+  de: {},
+  en: {}
+}
+
+type Language = keyof typeof translations
+let currentLanguage: Language = getDefaultLanguage() || "en"
+
+function getDefaultLanguage() {
+  const lang = navigator.languages ? navigator.languages[0] : navigator.language
+  const langShort = lang.substr(0, 2)
+
+  const availableTranslations = Object.keys(translations)
+  if (availableTranslations.indexOf(langShort) !== -1) {
+    return langShort as Language
+  } else {
+    return null
+  }
+}
+
+function createT(language: Language) {
+  const translation = translations[language]
+
+  const t = (key: string) => {
+    const keys = key.split(".")
+    const text = keys.reduce((obj: any, i) => {
+      return obj[i]
+    }, translation)
+
+    return text || key
+  }
+
+  return t
+}
+
+export function useTranslation() {
+  // wrap 't' in object since we can't have functions as state
+  const [t, setT] = React.useState<{ t: (key: string) => string }>({ t: createT(currentLanguage) })
+
+  const i18n = {
+    changeLanguage: (newLanguage: Language) => (currentLanguage = newLanguage)
+  }
+
+  React.useEffect(() => {
+    setT({ t: createT(currentLanguage) })
+  }, [currentLanguage])
+
+  return { t: t.t, i18n }
+}
