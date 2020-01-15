45c4bfe98649b3bf0a402fe5498eb9c1ddf4a54a
Marcel Ebert <mail@marcel-ebert.de>
2020-01-15 12:00:36 +0100

    Add resources to i18n initializiation

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,6 +2,8 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
 
+import translationEN from "../../i18n/locales/en/translation.json"
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
@@ -10,6 +12,11 @@ i18n
     fallbackLng: "en",
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
+    },
+    resources: {
+      en: {
+        translation: translationEN
+      }
     }
   })
 
