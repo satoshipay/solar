c2a9fae050fa16a3d030d61fee73226d96d298d5
Marcel Ebert <mail@marcel-ebert.de>
2020-01-15 15:37:06 +0100

    Add transactionRequest context

---
+++ src/App/contexts/transactionRequest.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import { StellarUri } from "@stellarguard/stellar-uri"
+import { verifyTransactionRequest } from "../../lib/stellar-uri"
+import { subscribeToDeepLinkURLs } from "../../platform/protocol-handler"
+import { trackError } from "./notifications"
+
+const allowUnsafeTestnetURIs = Boolean(process.env.ALLOW_UNSAFE_TESTNET_URIS)
+
+interface Props {
+  children: React.ReactNode
+}
+
+interface ContextType {
+  uri: StellarUri | null
+  clearURI: () => void
+}
+
+const initialValues: ContextType = {
+  uri: null,
+  clearURI: () => undefined
+}
+
+const TransactionRequestContext = React.createContext<ContextType>(initialValues)
+
+export function TransactionRequestProvider(props: Props) {
+  const [uri, setURI] = React.useState<StellarUri | null>(null)
+
+  const clearURI = React.useCallback(() => setURI(null), [])
+
+  React.useEffect(() => {
+    const unsubscribe = subscribeToDeepLinkURLs(async incomingURI => {
+      try {
+        const parsedURI = await verifyTransactionRequest(incomingURI, { allowUnsafeTestnetURIs })
+        setURI(parsedURI)
+      } catch (error) {
+        trackError(error)
+      }
+    })
+    return unsubscribe
+  }, [])
+
+  return (
+    <TransactionRequestContext.Provider value={{ uri, clearURI }}>{props.children}</TransactionRequestContext.Provider>
+  )
+}
+
+export { ContextType as TransactionRequestContextType, TransactionRequestContext }
