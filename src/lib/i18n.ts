28b3deaf5ffc5deb46ce33cd8ce2181f9e5e6ffc
Marcel Ebert <mail@marcel-ebert.de>
2020-01-20 16:51:56 +0100

    Move and rename translation.json to en.json

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
 
-import translationEN from "../../i18n/locales/en/translation.json"
+import translationEN from "../../i18n/locales/en.json"
 
 i18n
   .use(LanguageDetector)
