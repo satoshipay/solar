ed7760d2036c5978878996a85f115934eb5c4323
Marcel Ebert <marcel@wolfgang-ebert.de>
2020-01-20 17:56:15 +0100

    Add and configure 'i18next'  (#915)

---
+++ src/lib/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
+
+import translationEN from "../../i18n/locales/en.json"
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: process.env.NODE_ENV === "development" ? true : false,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false // not needed for react as it escapes by default
+    },
+    resources: {
+      en: {
+        translation: translationEN
+      }
+    }
+  })
+
+export default i18n
