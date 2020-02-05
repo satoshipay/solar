62b79b788646e125fd8164c372f37baf7f95ddfe
Marcel Ebert <mail@marcel-ebert.de>
2020-02-05 12:06:36 -0300

    Add ComplexError and getErrorTranslation

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,3 +1,18 @@
+import { TFunction } from "i18next"
+import pick from "lodash.pick"
+
+interface ComplexError extends Error {
+  __extraProps: string[]
+}
+
+export function ComplexError(name: string, message: string, extra?: Record<string, string>) {
+  return Object.assign(Error(message), {
+    ...extra,
+    name,
+    __extraProps: extra ? Object.keys(extra) : undefined
+  })
+}
+
 export interface Cancellation extends Error {
   name: "Cancellation"
 }
@@ -16,6 +31,26 @@ export function WrongPasswordError(message: string = "Wrong password.") {
 
 export function isWrongPasswordError(error: any) {
   return error && error.name === "WrongPasswordError"
+}
+
+function isComplexError(error: any): error is ComplexError {
+  return error && (error.name !== "Error" || error.__extraProps !== undefined)
+}
+
+function toKebabCase(value: string) {
+  return value
+    .replace(/([A-Z])([A-Z])/g, "$1-$2")
+    .replace(/([a-z])([A-Z])/g, "$1-$2")
+    .replace(/[\s_]+/g, "-")
+    .toLowerCase()
+}
+
+export function getErrorTranslation(error: Error, t: TFunction): string {
+  const key = `error.common.${toKebabCase(error.name)}`
+  const params = isComplexError(error) ? pick(error, error.__extraProps || []) : undefined
+
+  const fallback = error.message
+  return t([key, fallback], params)
 }
 
 export function renderFormFieldError(error: any) {
