274b8c6d12609626d2f0e16e92ad6a90f4a40f33
Marcel Ebert <mail@marcel-ebert.de>
2020-02-12 09:21:00 -0300

    Add serialization for CustomError

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,16 +1,73 @@
 import { TFunction } from "i18next"
 import pick from "lodash.pick"
+import { SerializerImplementation } from "threads"
 
-interface CustomError extends Error {
+interface SerializedCustomError {
   __extraProps: string[]
+  __type: "$$CustomError"
+  extras?: Record<string, string>
+  message: string
+  name: string
+  stack: any
 }
 
-export function CustomError(name: string, message: string, extra?: Record<string, string>) {
-  return Object.assign(Error(message), {
-    ...extra,
-    name,
-    __extraProps: extra ? Object.keys(extra) : undefined
+type CustomErrorProps = Record<string, string | number | boolean | null>
+
+type CustomError<ExtraProps extends CustomErrorProps = CustomErrorProps> = Error &
+  ExtraProps & {
+    __extraProps: string[]
+  }
+
+export function CustomError(name: string, message: string, extras?: CustomErrorProps): CustomError {
+  const error = Object.assign(Error(message), {
+    ...extras,
+    __extraProps: Object.keys(extras || {}),
+    name
+  }) as CustomError
+  return error
+}
+
+CustomError.deserialize = function deserializeCustomError(error: SerializedCustomError): CustomError {
+  return Object.assign(CustomError(error.name, error.message, pick(error, error.__extraProps || []) as any), {
+    stack: error.stack
   })
+}
+
+CustomError.serialize = function serializeCustomError(error: CustomError): SerializedCustomError {
+  return {
+    ...pick(error, error.__extraProps),
+    __extraProps: error.__extraProps,
+    __type: "$$CustomError",
+    message: error.message,
+    name: error.name,
+    stack: error.stack
+  }
+}
+
+CustomError.isCustomError = function isCustomError(thing: any): thing is CustomError {
+  return thing && thing instanceof Error && "__extraProps" in thing
+}
+
+function isSerializedCustomError(thing: any): thing is SerializedCustomError {
+  return thing && typeof thing === "object" && "__type" in thing && thing.__type === "$$CustomError"
+}
+
+export const CustomErrorSerializer: SerializerImplementation = {
+  deserialize(message, defaultHandler) {
+    if (isSerializedCustomError(message)) {
+      return CustomError.deserialize(message)
+    } else {
+      return defaultHandler(message)
+    }
+  },
+
+  serialize(thing, defaultHandler) {
+    if (CustomError.isCustomError(thing)) {
+      return CustomError.serialize(thing) as any
+    } else {
+      return defaultHandler(thing)
+    }
+  }
 }
 
 export interface Cancellation extends Error {
@@ -33,10 +90,6 @@ export function isWrongPasswordError(error: any) {
   return error && error.name === "WrongPasswordError"
 }
 
-function isCustomError(error: any): error is CustomError {
-  return error && (error.name !== "Error" || error.__extraProps !== undefined)
-}
-
 function toKebabCase(value: string) {
   return value
     .replace(/([A-Z])([A-Z])/g, "$1-$2")
@@ -47,7 +100,7 @@ function toKebabCase(value: string) {
 
 export function getErrorTranslation(error: Error, t: TFunction): string {
   const key = `error.${toKebabCase(error.name)}`
-  const params = isCustomError(error) ? pick(error, error.__extraProps || []) : undefined
+  const params = CustomError.isCustomError(error) ? pick(error, error.__extraProps || []) : undefined
 
   const fallback = error.message
   return t([key, fallback], params)
