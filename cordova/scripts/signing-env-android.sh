a7742b172fc3a25e8e50126af9d0f3aea5332c12
Andy Wermke <andy.dev@next-step-software.com>
2020-03-03 11:43:58 +0100

    Check and export some more Android build env vars

--- cordova/scripts/signing-env-android.sh
+++ cordova/scripts/signing-env-android.sh
@@ -7,12 +7,24 @@ if [ -f "./$ENV_FILE" ]; then
   source "./$ENV_FILE"
 fi
 
+if [ -z "$KEYSTORE_LOCATION" ]; then
+  echo "KEYSTORE_LOCATION not set"
+  exit 1
+fi
+
+if [ -z "$SIGNINGKEY_ALIAS" ]; then
+  echo "SIGNINGKEY_ALIAS not set"
+  exit 1
+fi
+
 echo "Keystore Password: "
 read -s KEYSTORE_PASSWORD
 
 echo "Key Password: "
 read -s SIGNINGKEY_PASSWORD
 
+export CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL
+export JAVA_HOME
 export KEYSTORE_LOCATION
 export KEYSTORE_PASSWORD
 export SIGNINGKEY_ALIAS
