df5dee58afc56e04c094598679e2784f93ca0027
fm612 <58794575+fm612@users.noreply.github.com>
2020-03-06 09:10:32 +0800

    Add files via upload

---
+++ i18n/zh-CN.ts
@@ -0,0 +1,17 @@
+import AccountSettings from "./locales/zh-CN/account-settings.json"
+import Account from "./locales/zh-CN/account.json"
+import AppSettings from "./locales/zh-CN/app-settings.json"
+import CreateAccount from "./locales/zh-CN/create-account.json"
+import Generic from "./locales/en/generic.json"
+import Operations from "./locales/zh-CN/operations.json"
+
+const translations = {
+  "account-settings": AccountSettings,
+  account: Account,
+  "app-settings": AppSettings,
+  "create-account": CreateAccount,
+  generic: Generic,
+  operations: Operations
+} as const
+
+export default translations
