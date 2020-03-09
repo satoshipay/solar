d71e8273f52c5704e9c12f273b8580a81b744e7a
Andy Wermke <andy@satoshipay.io>
2020-03-09 17:06:06 +0100

    Disable app.setAsDefaultProtocolClient (#995)
    Disable app.setAsDefaultProtocolClient() for now

--- electron/src/bootstrap.ts
+++ electron/src/bootstrap.ts
@@ -1,5 +1,7 @@
 import { app } from "electron"
+
 // Needs to match the value in electron-build.yml
 app.setAppUserModelId("io.solarwallet.app")
 
-app.setAsDefaultProtocolClient("web+stellar")
+// Disabled until we actually ship SEP-7 support
+// app.setAsDefaultProtocolClient("web+stellar")
