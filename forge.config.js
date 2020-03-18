5663959f7a0d59c5c712f499db706fce794f7013
Andy Wermke <github@andywer.com>
2020-03-18 18:15:48 +0100

    Make code signing work again. WIP
    Signed Windows build still failing.

--- forge.config.js
+++ forge.config.js
@@ -10,9 +10,16 @@ module.exports = {
     // macOS-specific options
     appBundleId: "io.solarwallet.app",
     appCategoryType: "public.app-category.finance",
+    osxNotarize: !process.env.APPLE_ID
+      ? undefined
+      : {
+          appleId: process.env.APPLE_ID,
+          appleIdPassword: process.env.APPLE_ID_PASSWORD
+        },
     osxSign: {
+      // TODO: Check if this works
       "entitlements-inherit": path.resolve(__dirname, "electron/build/entitlements.mac.inherit.plist"),
-      "hardened-runtime": true
+      hardenedRuntime: true
     },
     protocols: [
       {
@@ -47,6 +54,8 @@ module.exports = {
     {
       name: "@electron-forge/maker-squirrel",
       config: {
+        certificateFile: process.env.CSC_LINK || undefined,
+        certificatePassword: process.env.CSC_KEY_PASSWORD || undefined,
         setupIcon: path.resolve(__dirname, "electron/build/icon.ico"),
         loadingGif: path.resolve(__dirname, "electron/build/install-spinner.gif")
       }
@@ -54,6 +63,18 @@ module.exports = {
     {
       name: "@electron-forge/maker-zip",
       platforms: ["darwin"]
+    }
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        draft: true,
+        repository: {
+          owner: "satoshipay",
+          name: "solar"
+        }
+      }
     }
   ]
 }
--- scripts/signing-env.sh
+++ scripts/signing-env.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-PLATFORM="$1"
+PLATFORM="$1"   # "linux", "mac" or "win"
 ENV_FILE="signing-$PLATFORM.env"
 
 if [ -f "./$ENV_FILE" ]; then
   echo "Loading signing config from $ENV_FILE..."
   source "./$ENV_FILE"
+else
+  echo "Warning: Application binaries will not be signed."
 fi
 
-echo "Code Signing Password: "
-read -s CSC_KEY_PASSWORD
+if [ -f "./$ENV_FILE" ] && [ "$PLATFORM" = "win" ]; then
+  echo "Code Signing Password: "
+  read -s CSC_KEY_PASSWORD
+elif [ -f "./$ENV_FILE" ] && [ "$PLATFORM" = "mac" ] && [ -z "$APPLE_ID_PASSWORD" ]; then
+  echo "Password for Apple ID $APPLE_ID:"
+  read -s APPLE_ID_PASSWORD
+fi
 
+export APPLE_ID
+export APPLE_ID_PASSWORD
 export CSC_LINK
 export CSC_KEY_PASSWORD
-
-if [ "$PLATFORM" = "mac" ]; then
-  if [ -z "$APPLE_ID_PASSWORD" ]; then
-    echo "Password for Apple ID $APPLE_ID:"
-    read -s APPLE_ID_PASSWORD
-  fi
-
-  export APPLE_ID
-  export APPLE_ID_PASSWORD
-fi
