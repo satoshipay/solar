8bbc6a52e49ef1c7b2eea5fcc9aa963294fd7297
Andy Wermke <github@andywer.com>
2020-03-24 12:56:53 +0100

    Enable imports from src/* via ~*

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,6 @@
 const WorkerPlugin = require("@satoshipay/worker-plugin")
+const fs = require("fs")
+const path = require("path")
 
 module.exports = async function customizeWebpackConfig({ config }) {
   config.module.rules.push({
@@ -6,8 +8,21 @@ module.exports = async function customizeWebpackConfig({ config }) {
     loader: require.resolve("ts-loader")
   })
   config.output.globalObject = "self"
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    ...createModuleAliases()
+  }
   config.resolve.extensions.push(".ts", ".tsx")
   config.plugins.push(new WorkerPlugin({ preserveTypeModule: true }))
 
   return config
+}
+
+function createModuleAliases() {
+  const modules = fs.readdirSync(path.join(__dirname, "../src")).filter(filename => filename.match(/^[A-Z][^\.]+/))
+
+  return modules.reduce(
+    (aliases, moduleName) => ({ ...aliases, [`~${moduleName}`]: path.join(__dirname, `../src/${moduleName}`) }),
+    {}
+  )
 }
