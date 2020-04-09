59b58c871da046989357d195f3afef4d59f857b0
Marcel Ebert <mail@marcel-ebert.de>
2020-04-09 15:23:05 +0200

    Fix co-signing notification shown when awaiting

--- src/App/components/DesktopNotifications.tsx
+++ src/App/components/DesktopNotifications.tsx
@@ -109,16 +109,21 @@ function DesktopNotifications() {
   const handleNewSignatureRequest = React.useCallback(
     (signatureRequest: SignatureRequest) => {
       const signersHavingSigned = signatureRequest._embedded.signers.filter(signer => signer.has_signed)
+      const signersNotHavingSigned = signatureRequest._embedded.signers.filter(signer => !signer.has_signed)
+      const accountPublicKeys = accounts.map(account => account.publicKey)
 
-      showNotification(
-        {
-          title: "New transaction to co-sign",
-          text: `From ${signersHavingSigned.map(signer => signer.account_id).join(", ")}`
-        },
-        () => router.history.push(routes.allAccounts())
-      )
+      // only show notification when a local account has to co-sign
+      if (signersNotHavingSigned.some(signer => accountPublicKeys.includes(signer.account_id))) {
+        showNotification(
+          {
+            title: "New transaction to co-sign",
+            text: `From ${signersHavingSigned.map(signer => signer.account_id).join(", ")}`
+          },
+          () => router.history.push(routes.allAccounts())
+        )
+      }
     },
-    [router.history]
+    [accounts, router.history]
   )
 
   React.useEffect(() => {
