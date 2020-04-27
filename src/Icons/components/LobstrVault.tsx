65f4299d135ce3752b88236f26962984a198822e
Marcel Ebert <mail@marcel-ebert.de>
2020-04-27 15:38:52 +0200

    Change LobstrVault icon

--- src/Icons/components/LobstrVault.tsx
+++ src/Icons/components/LobstrVault.tsx
@@ -8,84 +8,59 @@ function LobstrVaultIcon(props: {
   style?: React.CSSProperties
 }) {
   return (
-    <SvgIcon viewBox="0 0 129 129" {...props}>
+    <SvgIcon viewBox="0 0 67 96" {...props}>
       <defs>
-        <linearGradient id="c" x1="81.523%" x2="14.838%" y1="19.257%" y2="100%">
-          <stop offset="0%" stopColor="#5116A4"></stop>
-          <stop offset="100%" stopColor="#9668FF"></stop>
-        </linearGradient>
-        <linearGradient id="d" x1="29.565%" x2="59.172%" y1="22.343%" y2="65.563%">
-          <stop offset="0%" stopColor="#FBF8FF"></stop>
-          <stop offset="100%" stopColor="#D0B1FF"></stop>
-        </linearGradient>
-        <linearGradient id="e" x1="74.917%" x2="0%" y1="88.177%" y2="11.038%">
+        <linearGradient id="linearGradient-1" x1="74.917%" x2="0%" y1="62.329%" y2="37.418%">
           <stop offset="0%"></stop>
           <stop offset="100%" stopColor="#090114" stopOpacity="0"></stop>
         </linearGradient>
-        <linearGradient id="f" x1="50%" x2="50%" y1="-10.027%" y2="10.132%">
-          <stop offset="0%" stopColor="#EEE"></stop>
-          <stop offset="100%" stopColor="#DAC1FF"></stop>
+        <linearGradient id="linearGradient-2" x1="74.917%" x2="0%" y1="88.177%" y2="11.038%">
+          <stop offset="0%"></stop>
+          <stop offset="100%" stopColor="#090114" stopOpacity="0"></stop>
         </linearGradient>
-        <linearGradient id="g" x1="0%" x2="50%" y1="42.075%" y2="42.075%">
-          <stop offset="0%" stopColor="#FFF"></stop>
-          <stop offset="100%" stopColor="#DBC3FF"></stop>
-        </linearGradient>
-        <linearGradient id="h" x1="36.036%" x2="63.964%" y1="100%" y2="42.066%">
-          <stop offset="0%" stopColor="#B27EFF"></stop>
-          <stop offset="100%" stopColor="#B482FF"></stop>
-        </linearGradient>
-        <linearGradient id="i" x1="0%" x2="50%" y1="42.075%" y2="42.075%">
-          <stop offset="0%" stopColor="#FFF"></stop>
-          <stop offset="100%" stopColor="#E8D8FF"></stop>
-        </linearGradient>
-        <linearGradient id="j" x1="0%" y1="53.823%" y2="46.959%">
-          <stop offset="0%" stopColor="#C7A1FF"></stop>
-          <stop offset="100%" stopColor="#BE92FF"></stop>
-        </linearGradient>
-        <path
-          id="a"
-          d="M44.975 0h39.05c12.95 0 19.424 0 26.396 2.204a27.397 27.397 0 0116.375 16.375C129 25.55 129 32.025 129 44.975v39.05c0 12.95 0 19.424-2.204 26.396a27.393 27.393 0 01-16.375 16.374C103.449 129 96.976 129 84.025 129h-39.05c-12.95 0-19.425 0-26.396-2.205a27.393 27.393 0 01-16.375-16.374C0 103.449 0 96.976 0 84.025v-39.05c0-12.95 0-19.425 2.204-26.396A27.397 27.397 0 0118.579 2.204C25.55 0 32.024 0 44.975 0"
-        ></path>
       </defs>
-      <g fill="none" fillRule="evenodd">
-        <mask id="b" fill="#fff">
-          <use xlinkHref="#a"></use>
-        </mask>
-        <use fill="#D6D6D6" xlinkHref="#a"></use>
-        <g mask="url(#b)">
-          <path fill="url(#c)" d="M0 0h129v129H0z"></path>
-          <path
-            fill="url(#d)"
-            d="M51.997 42.475c1.573-21.932-4.114-32.899-17.06-32.899-12.945 0-18.632 10.967-17.059 32.9H8.536C6.102 14.157 14.902 0 34.937 0 54.974 0 63.773 14.158 61.34 42.475h-9.342z"
-            transform="translate(30.1 13.975)"
-          ></path>
-          <path
-            fill="url(#e)"
-            fillOpacity="0.31"
-            d="M34.938 31.514l34.657 7.885c.373 14.557.373 24.471 0 29.742-.643 9.072-4.028 12.726-7.72 16.59-4.835 5.058-13.814 10.165-26.938 15.319C21.815 95.896 12.835 90.79 8 85.73 4.308 81.867.923 78.213.28 69.141c-.373-5.27-.373-15.185 0-29.742l34.657-7.885z"
-            transform="translate(30.1 13.975)"
-          ></path>
-          <path
-            fill="url(#f)"
-            d="M51.897 28.431C51.68 15.643 46.015 9.249 34.902 9.249c-11.114 0-16.755 6.394-16.924 19.182 1.898-11.363 7.539-17.044 16.924-17.044 9.384 0 15.05 5.681 16.995 17.044z"
-            transform="translate(30.1 13.975)"
-          ></path>
-          <path
-            fill="url(#g)"
-            d="M34.938 31.514l34.657 7.497c.373 13.84.373 23.265 0 28.277-.643 8.625-4.028 12.099-7.72 15.771-4.835 4.81-13.814 9.665-26.938 14.566C21.815 92.725 12.835 87.869 8 83.059 4.308 79.387.923 75.913.28 67.288c-.373-5.012-.373-14.437 0-28.277l34.657-7.497z"
-            transform="translate(30.1 13.975)"
-          ></path>
-          <path
-            fill="url(#h)"
-            d="M34.938 31.514l34.657 7.497c.373 13.84.373 23.265 0 28.277-.643 8.625-4.028 12.099-7.72 15.771-4.835 4.81-13.814 9.665-26.938 14.566V31.514z"
-            transform="translate(30.1 13.975)"
-          ></path>
-          <path fill="url(#i)" d="M34.77 31.514V37L0 44.075l.098-5.077z" transform="translate(30.1 13.975)"></path>
-          <path
-            fill="url(#j)"
-            d="M69.716 31.514v5.473l-34.778 7.088.113-5.069z"
-            transform="matrix(-1 0 0 1 134.754 13.975)"
-          ></path>
+      <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+        <g transform="translate(-28 -13)">
+          <g transform="translate(28.467 13.217)">
+            <path
+              fill="url(#linearGradient-1)"
+              fillOpacity="0.31"
+              d="M49.081 26.925C48.876 14.83 43.518 8.783 33.008 8.783c-10.51 0-15.846 6.047-16.006 18.142 1.795-10.747 7.13-16.12 16.006-16.12 8.875 0 14.233 5.373 16.073 16.12z"
+              opacity="0.8"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M7.822 37.256c-.069-.847.03-.139 0-.71C6.562 12.183 14.968 0 33.042 0c17.906 0 26.323 11.958 25.252 35.875-.063 1.391-.157-.092-.284 1.38m-41.102 0c-.115-1.876-.185-.345-.21-1.234-.502-17.976 4.946-26.964 16.344-26.964 11.436 0 16.882 9.048 16.338 27.145-.038 1.277-.107-.315-.205 1.054"
+              opacity="0.9"
+            ></path>
+            <path
+              fill="url(#linearGradient-2)"
+              fillOpacity="0.31"
+              d="M7.566 81.078C4.074 77.425.873 73.968.265 65.39c-.353-4.985-.353-14.36 0-28.128l32.777-7.457 32.776 7.457c.354 13.767.354 23.143 0 28.128-.608 8.58-3.809 12.036-7.3 15.69-4.573 4.784-13.065 9.613-25.476 14.488-12.411-4.875-20.903-9.704-25.476-14.489z"
+              opacity="0.8"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M7.566 78.552C4.074 75.08.873 71.793.265 63.636c-.353-4.739-.353-13.653 0-26.742l32.777-7.09 32.776 7.09c.354 13.089.354 22.003 0 26.742-.584 7.844-3.566 11.183-6.899 14.516l-.401.4c-4.573 4.55-13.065 9.14-25.476 13.775-12.411-4.634-20.903-9.226-25.476-13.775z"
+              opacity="0.8"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M33.042 29.804l32.776 7.09c.354 13.089.354 22.003 0 26.742-.608 8.157-3.809 11.443-7.3 14.916-4.573 4.55-13.065 9.14-25.476 13.775V29.804z"
+              opacity="0.5"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M33.0416667 29.8038418L33.0416667 34.9796962 -4.97379915e-14 41.6836021 0.0925333327 36.881422z"
+              opacity="0.9"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M65.9334084 29.8038418L65.9334084 34.9796962 33.0416667 41.6836021 33.148808 36.8893969z"
+              opacity="0.9"
+              transform="matrix(-1 0 0 1 98.975 0)"
+            ></path>
+          </g>
         </g>
       </g>
     </SvgIcon>
