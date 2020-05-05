fc68e7bd086e0b73eb70d7abb7d41a3513c220d3
Marcel Ebert <marcel@wolfgang-ebert.de>
2020-05-05 15:35:14 +0200

    Explain submission errors 500 and 504 (#1072)

--- src/Generic/lib/horizonErrors.ts
+++ src/Generic/lib/horizonErrors.ts
@@ -9,13 +9,14 @@ interface TxSubmissionResponse {
 // See <https://github.com/stellar/horizon/blob/master/src/github.com/stellar/horizon/codes/main.go>
 // See <https://www.stellar.org/developers/guides/concepts/transactions.html#possible-errors>
 export function explainSubmissionErrorResponse(response: TxSubmissionResponse | undefined, t: TFunction) {
-  if (!response || response.status !== 400) {
+  if (!response) {
     return CustomError("UnknownError", t("generic.error.unknown-error"))
   }
 
-  if (response.data && response.data.extras && response.data.extras.result_codes) {
-    // tslint:disable-next-line prefer-object-spread
-    const augment = (error: Error) => Object.assign(error, { response })
+  // tslint:disable-next-line prefer-object-spread
+  const augment = (error: Error) => Object.assign(error, { response })
+
+  if (response.status === 400 && response.data && response.data.extras && response.data.extras.result_codes) {
     const resultCodes = response.data.extras.result_codes
 
     if (resultCodes.operations && resultCodes.operations.length > 0) {
@@ -42,8 +43,11 @@ export function explainSubmissionErrorResponse(response: TxSubmissionResponse | 
         )
       )
     }
-
     // TODO: Handle more result codes
+  } else if (response.status === 500) {
+    return augment(Error(t("generic.error.submission-error.internal-server-error")))
+  } else if (response.status === 504) {
+    return augment(Error(t("generic.error.submission-error.timeout")))
   }
 
   return CustomError("UnknownError", t("generic.error.unknown-error"))
