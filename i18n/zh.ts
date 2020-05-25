513646f55185c775d2d88cff658fc135c05c0a82
Marcel Ebert <mail@marcel-ebert.de>
2020-05-25 10:33:58 +0200

    Add (empty) missing translation files for zh i18n

--- i18n/zh.ts
+++ i18n/zh.ts
@@ -5,18 +5,26 @@
 
 import AccountSettings from "./locales/zh/account-settings.json"
 import Account from "./locales/zh/account.json"
+import App from "./locales/zh/app.json"
 import AppSettings from "./locales/zh/app-settings.json"
 import CreateAccount from "./locales/zh/create-account.json"
 import Generic from "./locales/zh/generic.json"
 import Operations from "./locales/zh/operations.json"
+import Payment from "./locales/zh/payment.json"
+import Trading from "./locales/zh/trading.json"
+import TransferService from "./locales/zh/transfer-service.json"
 
 const translations = {
   "account-settings": AccountSettings,
   account: Account,
+  app: App,
   "app-settings": AppSettings,
   "create-account": CreateAccount,
   generic: Generic,
-  operations: Operations
+  operations: Operations,
+  payment: Payment,
+  trading: Trading,
+  "transfer-service": TransferService
 } as const
 
 export default translations
