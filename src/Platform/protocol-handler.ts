fa630c9aed801e4bdcab1a496cbdaec1b88a4a96
Marcel Ebert <mail@marcel-ebert.de>
2020-06-02 13:15:23 +0200

    Add helper functions for protocol handler state

--- src/Platform/protocol-handler.ts
+++ src/Platform/protocol-handler.ts
@@ -1,6 +1,18 @@
-import { subscribeToMessages } from "./ipc"
+import { call, subscribeToMessages } from "./ipc"
 import { Messages } from "../../shared/ipc"
 
 export function subscribeToDeepLinkURLs(callback: (url: string) => void) {
   return subscribeToMessages(Messages.DeepLinkURL, callback)
+}
+
+export function isDefaultProtocolClient() {
+  return call(Messages.IsDefaultProtocolClient)
+}
+
+export function isDifferentHandlerInstalled() {
+  return call(Messages.IsDifferentHandlerInstalled)
+}
+
+export function setAsDefaultProtocolClient() {
+  return call(Messages.SetAsDefaultProtocolClient)
 }
