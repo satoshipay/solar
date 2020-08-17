d3ed80d06880766c374eaef6cf5bcd5aca8ce168
Marcel Ebert <marcel@wolfgang-ebert.de>
2020-08-17 10:57:57 +0200

    Fix PasswordField storybook error (#1136)

--- src/Generic/components/PasswordField.tsx
+++ src/Generic/components/PasswordField.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import IconButton from "@material-ui/core/IconButton"
 import InputAdornment from "@material-ui/core/InputAdornment"
-import TextField, { TextFieldProps } from "@material-ui/core/TextField"
+import TextField, { StandardTextFieldProps } from "@material-ui/core/TextField"
 import Visibility from "@material-ui/icons/Visibility"
 import VisibilityOff from "@material-ui/icons/VisibilityOff"
 
-function PasswordField(props: Omit<TextFieldProps, "type">) {
+function PasswordField(props: Omit<StandardTextFieldProps, "type">) {
   const [showPassword, setShowPassword] = React.useState(false)
 
   const handleClickShowPassword = React.useCallback(() => {
