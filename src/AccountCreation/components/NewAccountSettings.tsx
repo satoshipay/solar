030d6cc23c1748701f9dcb7ffe61b14f9f355656
Marcel Ebert <mail@marcel-ebert.de>
2020-09-07 15:37:37 +0200

    Enable import of mnemonic phrase

--- src/AccountCreation/components/NewAccountSettings.tsx
+++ src/AccountCreation/components/NewAccountSettings.tsx
@@ -19,6 +19,13 @@ function NewAccountSettings(props: NewAccountSettingsProps) {
     onUpdateAccountCreation({ requiresPassword: !props.accountCreation.requiresPassword })
   }, [props.accountCreation.requiresPassword, onUpdateAccountCreation])
 
+  const toggleUseMnemonic = React.useCallback(
+    (useMnemonic: boolean) => {
+      onUpdateAccountCreation({ useMnemonic })
+    },
+    [onUpdateAccountCreation]
+  )
+
   const updatePassword = React.useCallback(
     (password: string) => {
       onUpdateAccountCreation({ password })
@@ -40,13 +47,24 @@ function NewAccountSettings(props: NewAccountSettingsProps) {
     [onUpdateAccountCreation]
   )
 
+  const updateMnemonic = React.useCallback(
+    (mnemonic: string) => {
+      onUpdateAccountCreation({ mnemonic })
+    },
+    [onUpdateAccountCreation]
+  )
+
   return (
     <List style={{ padding: isSmallScreen ? 0 : "24px 16px" }}>
       {props.accountCreation.import ? (
         <SecretKeyImport
           error={props.errors.secretKey}
           onEnterSecretKey={updateSecretKey}
+          onEnterMnemonic={updateMnemonic}
+          onToggleUseMnemonic={toggleUseMnemonic}
           secretKey={props.accountCreation.secretKey || ""}
+          mnemonic={props.accountCreation.mnemonic || ""}
+          useMnemonic={props.accountCreation.useMnemonic}
         />
       ) : null}
       <PasswordSetting
