5f628b56d7d016410e0efe06309bd2e13732fce4
Marcel Ebert <mail@marcel-ebert.de>
2020-09-07 13:08:28 +0200

    Add declarations for 'stellar-hd-wallet'

--- shared/types/platform.d.ts
+++ shared/types/platform.d.ts
@@ -24,3 +24,20 @@ interface BiometricAvailability {
   available: boolean
   enrolled: boolean
 }
+
+declare module "stellar-hd-wallet" {
+  class StellarHDWallet {
+    constructor(seedHex: string)
+
+    static fromMnemonic(mnemonic: string, password?: string, language?: string): StellarHDWallet
+    static generateMnemonic(entropyBits?: number, language?: string, rngFn?: Function): string
+    static fromSeed(seed: string | Buffer): StellarHDWallet
+    static validateMnemonic(mnemonic: string, language?: string): boolean
+
+    getKeypair(index: number): import("stellar-base").Keypair
+    getPublicKey(index: number): string
+    getSecret(index: number): string
+  }
+
+  export default StellarHDWallet
+}
