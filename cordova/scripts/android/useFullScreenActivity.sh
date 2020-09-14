5c8c49eec126592e798f4d186cde4842aed6dd68
Marcel Ebert <mail@marcel-ebert.de>
2020-09-14 11:46:56 +0200

    Fix android view not resizing with keyboard - Set resizeOnFullScreen to true to fix resizing issue - Add fix for navigation overflow and blackbar issue

--- cordova/scripts/android/useFullScreenActivity.sh
+++ cordova/scripts/android/useFullScreenActivity.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ROOT_DIR=$1
 cp $ROOT_DIR/scripts/android/MainActivity.java $ROOT_DIR/platforms/android/app/src/main/java/io/solarwallet/MainActivity.java
+cp $ROOT_DIR/scripts/android/CDVIonicKeyboard.java $ROOT_DIR/platforms/android/app/src/main/java/io/ionic/keyboard/CDVIonicKeyboard.java
