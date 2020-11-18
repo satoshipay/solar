5a7d94955e75808c7729250c48a1946095b0575a
Marcel Ebert <mail@marcel-ebert.de>
2020-11-18 11:01:26 +0100

    Remove 'will-navigate' listener The 'will-navigate' event is not emitted because links are opened programmatically And the 'new-window' event does not seem to be emitted when using `shell.openExternal()`

--- electron/src/window.ts
+++ electron/src/window.ts
@@ -62,16 +62,7 @@ export function createMainWindow() {
   // subscribes to window.open and <a target="_blank"></a> links and opens the url in the browser
   window.webContents.on("new-window", (event, url) => {
     event.preventDefault()
-    if (window.webContents.getURL().startsWith("file://")) {
-      shell.openExternal(url)
-    }
-  })
-
-  window.webContents.on("will-navigate", event => {
-    // limit navigation flows to unstrusted origins
-    if (!window.webContents.getURL().startsWith("file://")) {
-      event.preventDefault()
-    }
+    shell.openExternal(url)
   })
 
   // subscribe this window to deeplink urls
